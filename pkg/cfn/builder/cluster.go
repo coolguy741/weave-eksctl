eksctl-io/eksctl

Refactor the main API struct
This will accommodate for

- multiple nodegroups
- public and private subnets
- use taskmanager tasks for nodegroup(s) deletion
- cleanup nodegroup terminology

pkg/cfn/builder/cluster.go
@@ -1,19 +1,10 @@
 package builder
 
 import (
-	"net"
-
 	cfn "github.com/aws/aws-sdk-go/service/cloudformation"
 	gfn "github.com/awslabs/goformation/cloudformation"
 
 	"github.com/weaveworks/eksctl/pkg/eks/api"
-)
-
-const (
-	cfnOutputClusterCertificateAuthorityData = "CertificateAuthorityData"
-	cfnOutputClusterEndpoint                 = "Endpoint"
-	cfnOutputClusterARN                      = "ARN"
-	cfnOutputClusterStackName                = "ClusterStackName"
 )
 
 // ClusterResourceSet stores the resource information of the cluster
@@ -21,15 +12,17 @@ type ClusterResourceSet struct {
 	rs             *resourceSet
 	spec           *api.ClusterConfig
 	vpc            *gfn.Value
-	subnets        []*gfn.Value
+	subnets        map[api.SubnetTopology][]*gfn.Value
 	securityGroups []*gfn.Value
+	outputs        *ClusterStackOutputs
 }
 
 // NewClusterResourceSet returns a resource set for the new cluster
 func NewClusterResourceSet(spec *api.ClusterConfig) *ClusterResourceSet {
 	return &ClusterResourceSet{
-		rs:   newResourceSet(),
-		spec: spec,
+		rs:      newResourceSet(),
+		spec:    spec,
+		outputs: &ClusterStackOutputs{},
 	}
 }
 
@@ -38,18 +31,11 @@ func (c *ClusterResourceSet) AddAllResources() error {
 
 	templateDescriptionFeatures := clusterTemplateDescriptionDefaultFeatures
 
-	if c.spec.VPC != "" && len(c.spec.Subnets) >= 3 {
+	if c.spec.VPC.ID != "" && c.spec.VPC.HasSuffiecentPublicSubnets() {
 		c.importResourcesForVPC()
 		templateDescriptionFeatures = " (with shared VPC and dedicated IAM role) "
 	} else {
-		_, globalCIDR, _ := net.ParseCIDR("192.168.0.0/16")
-
-		subnets := map[string]*net.IPNet{}
-		_, subnets[c.spec.AvailabilityZones[0]], _ = net.ParseCIDR("192.168.64.0/18")
-		_, subnets[c.spec.AvailabilityZones[1]], _ = net.ParseCIDR("192.168.128.0/18")
-		_, subnets[c.spec.AvailabilityZones[2]], _ = net.ParseCIDR("192.168.192.0/18")
-
-		c.addResourcesForVPC(globalCIDR, subnets)
+		c.addResourcesForVPC()
 	}
 	c.addOutputsForVPC()
 
@@ -86,7 +72,7 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 		RoleArn: gfn.MakeFnGetAttString("ServiceRole.Arn"),
 		Version: gfn.NewString(version),
 		ResourcesVpcConfig: &gfn.AWSEKSCluster_ResourcesVpcConfig{
-			SubnetIds:        c.subnets,
+			SubnetIds:        c.subnets[api.SubnetTopologyPublic],
 			SecurityGroupIds: c.securityGroups,
 		},
 	})
@@ -98,5 +84,26 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 
 // GetAllOutputs collects all outputs of the cluster
 func (c *ClusterResourceSet) GetAllOutputs(stack cfn.Stack) error {
-	return c.rs.GetAllOutputs(stack, c.spec)
+	if err := c.rs.GetAllOutputs(stack, c.outputs); err != nil {
+		return err
+	}
+
+	c.spec.VPC.ID = c.outputs.VPC
+	c.spec.VPC.SecurityGroup = c.outputs.SecurityGroup
+
+	// TODO: shouldn't assume the order is the same, can probably do an API lookup
+	for i, subnet := range c.outputs.SubnetsPrivate {
+		c.spec.VPC.ImportSubnet(api.SubnetTopologyPrivate, c.spec.AvailabilityZones[i], subnet)
+	}
+
+	for i, subnet := range c.outputs.SubnetsPublic {
+		c.spec.VPC.ImportSubnet(api.SubnetTopologyPublic, c.spec.AvailabilityZones[i], subnet)
+	}
+
+	c.spec.ClusterStackName = c.outputs.ClusterStackName
+	c.spec.Endpoint = c.outputs.Endpoint
+	c.spec.CertificateAuthorityData = c.outputs.CertificateAuthorityData
+	c.spec.ARN = c.outputs.ARN
+
+	return nil
 }

pkg/eks/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net"
 	"time"
 
 	"github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
@@ -54,28 +55,11 @@ type ClusterConfig struct {
 	Tags        map[string]string
 	ClusterName string
 
-	NodeAMI  string
-	NodeType string
-	Nodes    int
-	MinNodes int
-	MaxNodes int
-
-	NodeVolumeSize int
-
-	MaxPodsPerNode int
-
-	NodePolicyARNs []string
-
-	NodeSSH          bool
-	SSHPublicKeyPath string
-	SSHPublicKey     []byte
-	SSHPublicKeyName string
-
 	WaitTimeout time.Duration
 
-	SecurityGroup string
-	Subnets       []string
-	VPC           string
+	VPC ClusterVPC
+
+	NodeGroups []*NodeGroup
 
 	Endpoint                 string
 	CertificateAuthorityData []byte
@@ -83,21 +67,156 @@ type ClusterConfig struct {
 
 	ClusterStackName string
 
-	NodeInstanceRoleARN string
-
 	AvailabilityZones []string
 
 	Addons ClusterAddons
 }
 
-// ClusterAddons provides addons for the created EKS cluster
-type ClusterAddons struct {
-	WithIAM AddonIAM
-	Storage bool
+// NewClusterConfig create new config for a cluster;
+// it doesn't include initial nodegroup, so user must
+// call NewNodeGroup to create one
+func NewClusterConfig() *ClusterConfig {
+	return &ClusterConfig{}
 }
 
-// AddonIAM provides an addon for the AWS IAM integration
-type AddonIAM struct {
-	PolicyAmazonEC2ContainerRegistryPowerUser bool
-	PolicyAutoScaling                         bool
+// SetSubnets defines CIDRs for each of the subnets,
+// it must be called after SetAvailabilityZones
+func (c *ClusterConfig) SetSubnets() {
+	_, c.VPC.CIDR, _ = net.ParseCIDR("192.168.0.0/16")
+
+	c.VPC.Subnets = map[SubnetTopology]map[string]Network{
+		SubnetTopologyPublic: map[string]Network{},
+	}
+
+	zoneCIDRs := []string{
+		"192.168.64.0/18",
+		"192.168.128.0/18",
+		"192.168.192.0/18",
+	}
+	for i, zone := range c.AvailabilityZones {
+		_, zoneCIDR, _ := net.ParseCIDR(zoneCIDRs[i])
+		c.VPC.Subnets[SubnetTopologyPublic][zone] = Network{
+			CIDR: zoneCIDR,
+		}
+	}
 }
+
+// NewNodeGroup crears new nodegroup inside cluster config,
+// it returns pointer to the nodegroup for convenience
+func (c *ClusterConfig) NewNodeGroup() *NodeGroup {
+	ng := &NodeGroup{
+		ID:             len(c.NodeGroups),
+		SubnetTopology: SubnetTopologyPublic,
+	}
+
+	c.NodeGroups = append(c.NodeGroups, ng)
+
+	return ng
+}
+
+// NodeGroup holds all configuration attributes that are
+// specific to a nodegroup
+type NodeGroup struct {
+	ID int
+
+	AMI               string
+	InstanceType      string
+	AvailabilityZones []string
+	Tags              map[string]string
+	SubnetTopology    SubnetTopology
+
+	DesiredCapacity int
+	MinSize         int
+	MaxSize         int
+
+	VolumeSize int
+
+	MaxPodsPerNode int
+
+	PolicyARNs      []string
+	InstanceRoleARN string
+
+	AllowSSH         bool
+	SSHPublicKeyPath string
+	SSHPublicKey     []byte
+	SSHPublicKeyName string
+}
+
+type (
+	// ClusterVPC holds global subnet and all child public/private subnet
+	ClusterVPC struct {
+		Network              // global CIRD and VPC ID
+		SecurityGroup string // cluster SG
+		// subnets are either public or private for use with separate nodegroups
+		// these are keyed by AZ for conveninece
+		Subnets map[SubnetTopology]map[string]Network
+		// for additional CIRD associations, e.g. to use with separate CIDR for
+		// private subnets or any ad-hoc subnets
+		ExtraCIDRs []*net.IPNet
+	}
+	// SubnetTopology can be SubnetTopologyPrivate or SubnetTopologyPublic
+	SubnetTopology string
+	// Network holds ID and CIDR
+	Network struct {
+		ID   string
+		CIDR *net.IPNet
+	}
+)
+
+const (
+	// SubnetTopologyPrivate repesents privately-routed subnets
+	SubnetTopologyPrivate SubnetTopology = "Private"
+	// SubnetTopologyPublic repesents publicly-routed subnets
+	SubnetTopologyPublic SubnetTopology = "Public"
+)
+
+// SubnetIDs returns list of subnets
+func (c ClusterVPC) SubnetIDs(topology SubnetTopology) []string {
+	subnets := []string{}
+	for _, s := range c.Subnets[topology] {
+		subnets = append(subnets, s.ID)
+	}
+	return subnets
+}
+
+// SubnetTopologies returns list of topologies supported
+// by a given cluster config
+func (c ClusterVPC) SubnetTopologies() []SubnetTopology {
+	topologies := []SubnetTopology{}
+	for topology := range c.Subnets {
+		topologies = append(topologies, topology)
+	}
+	return topologies
+}
+
+// ImportSubnet loads a given subnet into cluster config
+func (c ClusterVPC) ImportSubnet(topology SubnetTopology, az, subnetID string) {
+	if _, ok := c.Subnets[topology]; !ok {
+		c.Subnets[topology] = map[string]Network{}
+	}
+	if network, ok := c.Subnets[topology][az]; !ok {
+		c.Subnets[topology][az] = Network{ID: subnetID}
+	} else {
+		network.ID = subnetID
+		c.Subnets[topology][az] = network
+	}
+}
+
+// HasSuffiecentPublicSubnets validates if there is a suffiecent
+// number of subnets available to create a cluster
+func (c ClusterVPC) HasSuffiecentPublicSubnets() bool {
+	return len(c.SubnetIDs(SubnetTopologyPublic)) >= 3
+}
+
+type (
+	// ClusterAddons provides addons for the created EKS cluster
+	ClusterAddons struct {
+		WithIAM AddonIAM
+		Storage bool
+	}
+	// AddonIAM provides an addon for the AWS IAM integration
+	AddonIAM struct {
+		PolicyAmazonEC2ContainerRegistryPowerUser bool
+		PolicyAutoScaling                         bool
+	}
+)

pkg/kops/kops.go
@@ -55,20 +55,20 @@ func (k *Wrapper) UseVPC(spec *api.ClusterConfig) error {
 	if len(vpcs) > 1 {
 		return fmt.Errorf("more then one VPC found for kops cluster %q", k.clusterName)
 	}
-	spec.VPC = vpcs[0]
+	spec.VPC.ID = vpcs[0]
 
 	for _, subnet := range allSubnets {
 		subnet := subnet.Obj.(*ec2.Subnet)
 		for _, tag := range subnet.Tags {
 			if k.isOwned(tag) && *subnet.VpcId == vpcs[0] {
-				spec.Subnets = append(spec.Subnets, *subnet.SubnetId)
+				spec.VPC.ImportSubnet(api.SubnetTopologyPublic, *subnet.AvailabilityZone, *subnet.SubnetId)
 				spec.AvailabilityZones = append(spec.AvailabilityZones, *subnet.AvailabilityZone)
 			}
 		}
 	}
-	logger.Debug("subnets = %#v", spec.Subnets)
-	if len(spec.Subnets) < 3 {
-		return fmt.Errorf("cannot use VPC from kops cluster less then 3 subnets")
+	logger.Debug("subnets = %#v", spec.VPC.Subnets)
+	if !spec.VPC.HasSuffiecentPublicSubnets() {
+		return fmt.Errorf("cannot use VPC from kops cluster with less then 3 subnets")
 	}
 
 	return nil

coolguy741/weave-eksctl

pkg/cfn/builder/cluster.go
@@ -1,19 +1,10 @@
 package builder
 
 import (
-	"net"
-
 	cfn "github.com/aws/aws-sdk-go/service/cloudformation"
 	gfn "github.com/awslabs/goformation/cloudformation"
 
 	"github.com/weaveworks/eksctl/pkg/eks/api"
-)
-
-const (
-	cfnOutputClusterCertificateAuthorityData = "CertificateAuthorityData"
-	cfnOutputClusterEndpoint                 = "Endpoint"
-	cfnOutputClusterARN                      = "ARN"
-	cfnOutputClusterStackName                = "ClusterStackName"
 )
 
 // ClusterResourceSet stores the resource information of the cluster
@@ -21,15 +12,17 @@ type ClusterResourceSet struct {
 	rs             *resourceSet
 	spec           *api.ClusterConfig
 	vpc            *gfn.Value
-	subnets        []*gfn.Value
+	subnets        map[api.SubnetTopology][]*gfn.Value
 	securityGroups []*gfn.Value
+	outputs        *ClusterStackOutputs
 }
 
 // NewClusterResourceSet returns a resource set for the new cluster
 func NewClusterResourceSet(spec *api.ClusterConfig) *ClusterResourceSet {
 	return &ClusterResourceSet{
-		rs:   newResourceSet(),
-		spec: spec,
+		rs:      newResourceSet(),
+		spec:    spec,
+		outputs: &ClusterStackOutputs{},
 	}
 }
 
@@ -38,18 +31,11 @@ func (c *ClusterResourceSet) AddAllResources() error {
 
 	templateDescriptionFeatures := clusterTemplateDescriptionDefaultFeatures
 
-	if c.spec.VPC != "" && len(c.spec.Subnets) >= 3 {
+	if c.spec.VPC.ID != "" && c.spec.VPC.HasSufficientPublicSubnets() {
 		c.importResourcesForVPC()
 		templateDescriptionFeatures = " (with shared VPC and dedicated IAM role) "
 	} else {
-		_, globalCIDR, _ := net.ParseCIDR("192.168.0.0/16")
-
-		subnets := map[string]*net.IPNet{}
-		_, subnets[c.spec.AvailabilityZones[0]], _ = net.ParseCIDR("192.168.64.0/18")
-		_, subnets[c.spec.AvailabilityZones[1]], _ = net.ParseCIDR("192.168.128.0/18")
-		_, subnets[c.spec.AvailabilityZones[2]], _ = net.ParseCIDR("192.168.192.0/18")
-
-		c.addResourcesForVPC(globalCIDR, subnets)
+		c.addResourcesForVPC()
 	}
 	c.addOutputsForVPC()
 
@@ -86,7 +72,7 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 		RoleArn: gfn.MakeFnGetAttString("ServiceRole.Arn"),
 		Version: gfn.NewString(version),
 		ResourcesVpcConfig: &gfn.AWSEKSCluster_ResourcesVpcConfig{
-			SubnetIds:        c.subnets,
+			SubnetIds:        c.subnets[api.SubnetTopologyPublic],
 			SecurityGroupIds: c.securityGroups,
 		},
 	})
@@ -98,5 +84,26 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 
 // GetAllOutputs collects all outputs of the cluster
 func (c *ClusterResourceSet) GetAllOutputs(stack cfn.Stack) error {
-	return c.rs.GetAllOutputs(stack, c.spec)
+	if err := c.rs.GetAllOutputs(stack, c.outputs); err != nil {
+		return err
+	}
+
+	c.spec.VPC.ID = c.outputs.VPC
+	c.spec.VPC.SecurityGroup = c.outputs.SecurityGroup
+
+	// TODO: shouldn't assume the order is the same, can probably do an API lookup
+	for i, subnet := range c.outputs.SubnetsPrivate {
+		c.spec.VPC.ImportSubnet(api.SubnetTopologyPrivate, c.spec.AvailabilityZones[i], subnet)
+	}
+
+	for i, subnet := range c.outputs.SubnetsPublic {
+		c.spec.VPC.ImportSubnet(api.SubnetTopologyPublic, c.spec.AvailabilityZones[i], subnet)
+	}
+
+	c.spec.ClusterStackName = c.outputs.ClusterStackName
+	c.spec.Endpoint = c.outputs.Endpoint
+	c.spec.CertificateAuthorityData = c.outputs.CertificateAuthorityData
+	c.spec.ARN = c.outputs.ARN
+
+	return nil
 }

pkg/eks/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net"
 	"time"
 
 	"github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
@@ -54,28 +55,11 @@ type ClusterConfig struct {
 	Tags        map[string]string
 	ClusterName string
 
-	NodeAMI  string
-	NodeType string
-	Nodes    int
-	MinNodes int
-	MaxNodes int
-
-	NodeVolumeSize int
-
-	MaxPodsPerNode int
-
-	NodePolicyARNs []string
-
-	NodeSSH          bool
-	SSHPublicKeyPath string
-	SSHPublicKey     []byte
-	SSHPublicKeyName string
-
 	WaitTimeout time.Duration
 
-	SecurityGroup string
-	Subnets       []string
-	VPC           string
+	VPC ClusterVPC
+
+	NodeGroups []*NodeGroup
 
 	Endpoint                 string
 	CertificateAuthorityData []byte
@@ -83,21 +67,156 @@ type ClusterConfig struct {
 
 	ClusterStackName string
 
-	NodeInstanceRoleARN string
-
 	AvailabilityZones []string
 
 	Addons ClusterAddons
 }
 
-// ClusterAddons provides addons for the created EKS cluster
-type ClusterAddons struct {
-	WithIAM AddonIAM
-	Storage bool
+// NewClusterConfig create new config for a cluster;
+// it doesn't include initial nodegroup, so user must
+// call NewNodeGroup to create one
+func NewClusterConfig() *ClusterConfig {
+	return &ClusterConfig{}
 }
 
-// AddonIAM provides an addon for the AWS IAM integration
-type AddonIAM struct {
-	PolicyAmazonEC2ContainerRegistryPowerUser bool
-	PolicyAutoScaling                         bool
+// SetSubnets defines CIDRs for each of the subnets,
+// it must be called after SetAvailabilityZones
+func (c *ClusterConfig) SetSubnets() {
+	_, c.VPC.CIDR, _ = net.ParseCIDR("192.168.0.0/16")
+
+	c.VPC.Subnets = map[SubnetTopology]map[string]Network{
+		SubnetTopologyPublic: map[string]Network{},
+	}
+
+	zoneCIDRs := []string{
+		"192.168.64.0/18",
+		"192.168.128.0/18",
+		"192.168.192.0/18",
+	}
+	for i, zone := range c.AvailabilityZones {
+		_, zoneCIDR, _ := net.ParseCIDR(zoneCIDRs[i])
+		c.VPC.Subnets[SubnetTopologyPublic][zone] = Network{
+			CIDR: zoneCIDR,
+		}
+	}
 }
+
+// NewNodeGroup crears new nodegroup inside cluster config,
+// it returns pointer to the nodegroup for convenience
+func (c *ClusterConfig) NewNodeGroup() *NodeGroup {
+	ng := &NodeGroup{
+		ID:             len(c.NodeGroups),
+		SubnetTopology: SubnetTopologyPublic,
+	}
+
+	c.NodeGroups = append(c.NodeGroups, ng)
+
+	return ng
+}
+
+// NodeGroup holds all configuration attributes that are
+// specific to a nodegroup
+type NodeGroup struct {
+	ID int
+
+	AMI               string
+	InstanceType      string
+	AvailabilityZones []string
+	Tags              map[string]string
+	SubnetTopology    SubnetTopology
+
+	DesiredCapacity int
+	MinSize         int
+	MaxSize         int
+
+	VolumeSize int
+
+	MaxPodsPerNode int
+
+	PolicyARNs      []string
+	InstanceRoleARN string
+
+	AllowSSH         bool
+	SSHPublicKeyPath string
+	SSHPublicKey     []byte
+	SSHPublicKeyName string
+}
+
+type (
+	// ClusterVPC holds global subnet and all child public/private subnet
+	ClusterVPC struct {
+		Network              // global CIRD and VPC ID
+		SecurityGroup string // cluster SG
+		// subnets are either public or private for use with separate nodegroups
+		// these are keyed by AZ for conveninece
+		Subnets map[SubnetTopology]map[string]Network
+		// for additional CIRD associations, e.g. to use with separate CIDR for
+		// private subnets or any ad-hoc subnets
+		ExtraCIDRs []*net.IPNet
+	}
+	// SubnetTopology can be SubnetTopologyPrivate or SubnetTopologyPublic
+	SubnetTopology string
+	// Network holds ID and CIDR
+	Network struct {
+		ID   string
+		CIDR *net.IPNet
+	}
+)
+
+const (
+	// SubnetTopologyPrivate repesents privately-routed subnets
+	SubnetTopologyPrivate SubnetTopology = "Private"
+	// SubnetTopologyPublic repesents publicly-routed subnets
+	SubnetTopologyPublic SubnetTopology = "Public"
+)
+
+// SubnetIDs returns list of subnets
+func (c ClusterVPC) SubnetIDs(topology SubnetTopology) []string {
+	subnets := []string{}
+	for _, s := range c.Subnets[topology] {
+		subnets = append(subnets, s.ID)
+	}
+	return subnets
+}
+
+// SubnetTopologies returns list of topologies supported
+// by a given cluster config
+func (c ClusterVPC) SubnetTopologies() []SubnetTopology {
+	topologies := []SubnetTopology{}
+	for topology := range c.Subnets {
+		topologies = append(topologies, topology)
+	}
+	return topologies
+}
+
+// ImportSubnet loads a given subnet into cluster config
+func (c ClusterVPC) ImportSubnet(topology SubnetTopology, az, subnetID string) {
+	if _, ok := c.Subnets[topology]; !ok {
+		c.Subnets[topology] = map[string]Network{}
+	}
+	if network, ok := c.Subnets[topology][az]; !ok {
+		c.Subnets[topology][az] = Network{ID: subnetID}
+	} else {
+		network.ID = subnetID
+		c.Subnets[topology][az] = network
+	}
+}
+
+// HasSufficientPublicSubnets validates if there is a suffiecent
+// number of subnets available to create a cluster
+func (c ClusterVPC) HasSufficientPublicSubnets() bool {
+	return len(c.SubnetIDs(SubnetTopologyPublic)) >= 3
+}
+
+type (
+	// ClusterAddons provides addons for the created EKS cluster
+	ClusterAddons struct {
+		WithIAM AddonIAM
+		Storage bool
+	}
+	// AddonIAM provides an addon for the AWS IAM integration
+	AddonIAM struct {
+		PolicyAmazonEC2ContainerRegistryPowerUser bool
+		PolicyAutoScaling                         bool
+	}
+)

pkg/kops/kops.go
@@ -55,20 +55,20 @@ func (k *Wrapper) UseVPC(spec *api.ClusterConfig) error {
 	if len(vpcs) > 1 {
 		return fmt.Errorf("more then one VPC found for kops cluster %q", k.clusterName)
 	}
-	spec.VPC = vpcs[0]
+	spec.VPC.ID = vpcs[0]
 
 	for _, subnet := range allSubnets {
 		subnet := subnet.Obj.(*ec2.Subnet)
 		for _, tag := range subnet.Tags {
 			if k.isOwned(tag) && *subnet.VpcId == vpcs[0] {
-				spec.Subnets = append(spec.Subnets, *subnet.SubnetId)
+				spec.VPC.ImportSubnet(api.SubnetTopologyPublic, *subnet.AvailabilityZone, *subnet.SubnetId)
 				spec.AvailabilityZones = append(spec.AvailabilityZones, *subnet.AvailabilityZone)
 			}
 		}
 	}
-	logger.Debug("subnets = %#v", spec.Subnets)
-	if len(spec.Subnets) < 3 {
-		return fmt.Errorf("cannot use VPC from kops cluster less then 3 subnets")
+	logger.Debug("subnets = %#v", spec.VPC.Subnets)
+	if !spec.VPC.HasSufficientPublicSubnets() {
+		return fmt.Errorf("cannot use VPC from kops cluster with less then 3 subnets")
 	}
 
 	return nil