djosephsen/eksctl

Clear separation between `api.ClusterConfig` and `api.ClusterProvider`
This refactors how we separate provider configuration and it's recievers
from cluster configuration. We no longer copy cluster configuration in
every places where we need it, instead we pass it explicitly every time.

pkg/cfn/builder/cluster.go
@@ -79,7 +79,7 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 	}
 
 	c.newResource("ControlPlane", &gfn.AWSEKSCluster{
-		Name:               gfn.NewString(c.spec.ClusterName),
+		Name:               gfn.NewString(c.spec.ID.Name),
 		RoleArn:            gfn.MakeFnGetAttString("ServiceRole.Arn"),
 		Version:            gfn.NewString(version),
 		ResourcesVpcConfig: clusterVPC,

pkg/cfn/builder/nodegroup.go
@@ -33,7 +33,7 @@ func NewNodeGroupResourceSet(spec *api.ClusterConfig, clusterStackName string, i
 		rs:               newResourceSet(),
 		id:               id,
 		clusterStackName: clusterStackName,
-		nodeGroupName:    fmt.Sprintf("%s-%d", spec.ClusterName, id),
+		nodeGroupName:    fmt.Sprintf("%s-%d", spec.ID.Name, id),
 		clusterSpec:      spec,
 		spec:             spec.NodeGroups[id],
 	}
@@ -156,7 +156,7 @@ func (n *NodeGroupResourceSet) addResourcesForNodeGroup() error {
 			"VPCZoneIdentifier":       vpcZoneIdentifier,
 			"Tags": []map[string]interface{}{
 				{"Key": "Name", "Value": fmt.Sprintf("%s-Node", n.nodeGroupName), "PropagateAtLaunch": "true"},
-				{"Key": "kubernetes.io/cluster/" + n.clusterSpec.ClusterName, "Value": "owned", "PropagateAtLaunch": "true"},
+				{"Key": "kubernetes.io/cluster/" + n.clusterSpec.ID.Name, "Value": "owned", "PropagateAtLaunch": "true"},
 			},
 		},
 		UpdatePolicy: map[string]map[string]string{

pkg/cfn/manager/api.go
@@ -36,9 +36,10 @@ type ChangeSet = cloudformation.DescribeChangeSetOutput
 
 // StackCollection stores the CloudFormation stack information
 type StackCollection struct {
-	cfn  cloudformationiface.CloudFormationAPI
-	spec *api.ClusterConfig
-	tags []*cloudformation.Tag
+	cfn         cloudformationiface.CloudFormationAPI
+	waitTimeout time.Duration
+	spec        *api.ClusterConfig
+	tags        []*cloudformation.Tag
 }
 
 func newTag(key, value string) *cloudformation.Tag {
@@ -48,16 +49,17 @@ func newTag(key, value string) *cloudformation.Tag {
 // NewStackCollection create a stack manager for a single cluster
 func NewStackCollection(provider api.ClusterProvider, spec *api.ClusterConfig) *StackCollection {
 	tags := []*cloudformation.Tag{
-		newTag(ClusterNameTag, spec.ClusterName),
+		newTag(ClusterNameTag, spec.ID.Name),
 	}
 	for key, value := range spec.Tags {
 		tags = append(tags, newTag(key, value))
 	}
 	logger.Debug("tags = %#v", tags)
 	return &StackCollection{
-		cfn:  provider.CloudFormation(),
-		spec: spec,
-		tags: tags,
+		cfn:         provider.CloudFormation(),
+		waitTimeout: provider.WaitTimeout(),
+		spec:        spec,
+		tags:        tags,
 	}
 }
 
@@ -203,7 +205,7 @@ func (c *StackCollection) DeleteStack(name string) (*Stack, error) {
 	}
 	i.StackId = s.StackId
 	for _, tag := range s.Tags {
-		if *tag.Key == ClusterNameTag && *tag.Value == c.spec.ClusterName {
+		if *tag.Key == ClusterNameTag && *tag.Value == c.spec.ID.Name {
 			input := &cloudformation.DeleteStackInput{
 				StackName: i.StackId,
 			}
@@ -217,7 +219,7 @@ func (c *StackCollection) DeleteStack(name string) (*Stack, error) {
 	}
 
 	return nil, fmt.Errorf("cannot delete stack %q as it doesn't bare our %q tag", *s.StackName,
-		fmt.Sprintf("%s:%s", ClusterNameTag, c.spec.ClusterName))
+		fmt.Sprintf("%s:%s", ClusterNameTag, c.spec.ID.Name))
 }
 
 // WaitDeleteStack kills a stack by name and waits for DELETED status

pkg/cfn/manager/cluster.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (c *StackCollection) makeClusterStackName() string {
-	return "eksctl-" + c.spec.ClusterName + "-cluster"
+	return "eksctl-" + c.spec.ID.Name + "-cluster"
 }
 
 // CreateCluster creates the cluster

pkg/ctl/get/cluster.go
@@ -15,6 +15,7 @@ import (
 var listAllRegions bool
 
 func getClusterCmd() *cobra.Command {
+	p := &api.ProviderConfig{}
 	cfg := api.NewClusterConfig()
 
 	cmd := &cobra.Command{
@@ -22,7 +23,7 @@ func getClusterCmd() *cobra.Command {
 		Short:   "Get cluster(s)",
 		Aliases: []string{"clusters"},
 		Run: func(_ *cobra.Command, args []string) {
-			if err := doGetCluster(cfg, ctl.GetNameArg(args)); err != nil {
+			if err := doGetCluster(p, cfg, ctl.GetNameArg(args)); err != nil {
 				logger.Critical("%s\n", err.Error())
 				os.Exit(1)
 			}
@@ -31,38 +32,39 @@ func getClusterCmd() *cobra.Command {
 
 	fs := cmd.Flags()
 
-	fs.StringVarP(&cfg.ClusterName, "name", "n", "", "EKS cluster name")
+	fs.StringVarP(&cfg.ID.Name, "name", "n", "", "EKS cluster name")
 	fs.BoolVarP(&listAllRegions, "all-regions", "A", false, "List clusters across all supported regions")
 	fs.IntVar(&chunkSize, "chunk-size", defaultChunkSize, "Return large lists in chunks rather than all at once. Pass 0 to disable.")
 
-	fs.StringVarP(&cfg.Region, "region", "r", "", "AWS region")
-	fs.StringVarP(&cfg.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
+	fs.StringVarP(&p.Region, "region", "r", "", "AWS region")
+	fs.StringVarP(&p.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
 
 	fs.StringVarP(&output, "output", "o", "table", "Specifies the output format. Choose from table,json,yaml. Defaults to table.")
+
 	return cmd
 }
 
-func doGetCluster(cfg *api.ClusterConfig, name string) error {
-	regionGiven := cfg.Region != "" // eks.New resets this field, so we need to check if it was set in the fist place
-	ctl := eks.New(cfg)
+func doGetCluster(p *api.ProviderConfig, cfg *api.ClusterConfig, name string) error {
+	regionGiven := cfg.ID.Region != "" // eks.New resets this field, so we need to check if it was set in the fist place
+	ctl := eks.New(p, cfg)
 
-	if !cfg.IsSupportedRegion() {
-		return fmt.Errorf("--region=%s is not supported - use one of: %s", cfg.Region, strings.Join(api.SupportedRegions(), ", "))
+	if !ctl.IsSupportedRegion() {
+		return fmt.Errorf("--region=%s is not supported - use one of: %s", cfg.ID.Region, strings.Join(api.SupportedRegions(), ", "))
 	}
 
 	if regionGiven && listAllRegions {
-		logger.Warning("--region=%s is ignored, as --all-regions is given", cfg.Region)
+		logger.Warning("--region=%s is ignored, as --all-regions is given", cfg.ID.Region)
 	}
 
-	if cfg.ClusterName != "" && name != "" {
-		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ClusterName, name)
+	if cfg.ID.Name != "" && name != "" {
+		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ID.Name, name)
 	}
 
 	if name != "" {
-		cfg.ClusterName = name
+		cfg.ID.Name = name
 	}
 
-	if cfg.ClusterName != "" && listAllRegions {
+	if cfg.ID.Name != "" && listAllRegions {
 		return fmt.Errorf("--all-regions is for listing all clusters, it must be used without cluster name flag/argument")
 	}
 
@@ -70,5 +72,5 @@ func doGetCluster(cfg *api.ClusterConfig, name string) error {
 		return err
 	}
 
-	return ctl.ListClusters(chunkSize, output, listAllRegions)
+	return ctl.ListClusters(cfg.ID.Name, chunkSize, output, listAllRegions)
 }

pkg/ctl/utils/write_kubeconfig.go
@@ -21,13 +21,14 @@ var (
 )
 
 func writeKubeconfigCmd() *cobra.Command {
+	p := &api.ProviderConfig{}
 	cfg := api.NewClusterConfig()
 
 	cmd := &cobra.Command{
 		Use:   "write-kubeconfig",
 		Short: "Write kubeconfig file for a given cluster",
 		Run: func(_ *cobra.Command, args []string) {
-			if err := doWriteKubeconfigCmd(cfg, ctl.GetNameArg(args)); err != nil {
+			if err := doWriteKubeconfigCmd(p, cfg, ctl.GetNameArg(args)); err != nil {
 				logger.Critical("%s\n", err.Error())
 				os.Exit(1)
 			}
@@ -36,10 +37,10 @@ func writeKubeconfigCmd() *cobra.Command {
 
 	fs := cmd.Flags()
 
-	fs.StringVarP(&cfg.ClusterName, "name", "n", "", "EKS cluster name (required)")
+	fs.StringVarP(&cfg.ID.Name, "name", "n", "", "EKS cluster name (required)")
 
-	fs.StringVarP(&cfg.Region, "region", "r", "", "AWS region")
-	fs.StringVarP(&cfg.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
+	fs.StringVarP(&p.Region, "region", "r", "", "AWS region")
+	fs.StringVarP(&p.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
 
 	fs.BoolVar(&utilsAutoKubeconfigPath, "auto-kubeconfig", false, fmt.Sprintf("save kubeconfig file by cluster name – %q", kubeconfig.AutoPath("<name>")))
 	fs.StringVar(&utilsKubeconfigOutputPath, "kubeconfig", kubeconfig.DefaultPath, "path to write kubeconfig")
@@ -48,22 +49,22 @@ func writeKubeconfigCmd() *cobra.Command {
 	return cmd
 }
 
-func doWriteKubeconfigCmd(cfg *api.ClusterConfig, name string) error {
-	ctl := eks.New(cfg)
+func doWriteKubeconfigCmd(p *api.ProviderConfig, cfg *api.ClusterConfig, name string) error {
+	ctl := eks.New(p, cfg)
 
 	if err := ctl.CheckAuth(); err != nil {
 		return err
 	}
 
-	if cfg.ClusterName != "" && name != "" {
-		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ClusterName, name)
+	if cfg.ID.Name != "" && name != "" {
+		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ID.Name, name)
 	}
 
 	if name != "" {
-		cfg.ClusterName = name
+		cfg.ID.Name = name
 	}
 
-	if cfg.ClusterName == "" {
+	if cfg.ID.Name == "" {
 		return fmt.Errorf("--name must be set")
 	}
 
@@ -71,21 +72,21 @@ func doWriteKubeconfigCmd(cfg *api.ClusterConfig, name string) error {
 		if utilsKubeconfigOutputPath != kubeconfig.DefaultPath {
 			return fmt.Errorf("--kubeconfig and --auto-kubeconfig cannot be used at the same time")
 		}
-		utilsKubeconfigOutputPath = kubeconfig.AutoPath(cfg.ClusterName)
+		utilsKubeconfigOutputPath = kubeconfig.AutoPath(cfg.ID.Name)
 	}
 
-	cluster, err := ctl.DescribeControlPlane()
+	cluster, err := ctl.DescribeControlPlane(cfg.ID)
 	if err != nil {
 		return err
 	}
 
 	logger.Debug("cluster = %#v", cluster)
 
-	if err = ctl.GetCredentials(*cluster); err != nil {
+	if err = ctl.GetCredentials(*cluster, cfg); err != nil {
 		return err
 	}
 
-	clientConfigBase, err := ctl.NewClientConfig()
+	clientConfigBase, err := ctl.NewClientConfig(cfg)
 	if err != nil {
 		return err
 	}

pkg/utils/kubeconfig/kubeconfig.go
@@ -31,7 +31,7 @@ const (
 // if certificateAuthorityPath is no empty, it is used instead of
 // embedded certificate-authority-data
 func New(spec *api.ClusterConfig, username, certificateAuthorityPath string) (*clientcmdapi.Config, string, string) {
-	clusterName := getCompleteClusterName(spec)
+	clusterName := spec.ID.String()
 	contextName := fmt.Sprintf("%s@%s", username, clusterName)
 
 	c := &clientcmdapi.Config{
@@ -66,7 +66,7 @@ func AppendAuthenticator(c *clientcmdapi.Config, spec *api.ClusterConfig, comman
 	c.AuthInfos[c.CurrentContext].Exec = &clientcmdapi.ExecConfig{
 		APIVersion: "client.authentication.k8s.io/v1alpha1",
 		Command:    command,
-		Args:       []string{"token", "-i", spec.ClusterName},
+		Args:       []string{"token", "-i", spec.ID.Name},
 		/*
 			Args:       []string{"token", "-i", c.Cluster.ClusterName, "-r", c.roleARN},
 		*/
@@ -98,10 +98,6 @@ func Write(path string, newConfig *clientcmdapi.Config, setContext bool) (string
 	}
 
 	return configAccess.GetDefaultFilename(), nil
-}
-
-func getCompleteClusterName(spec *api.ClusterConfig) string {
-	return fmt.Sprintf("%s.%s.eksctl.io", spec.ClusterName, spec.Region)
 }
 
 func getConfigAccess(explicitPath string) clientcmd.ConfigAccess {
@@ -157,8 +153,8 @@ func isValidConfig(p, name string) error {
 }
 
 // MaybeDeleteConfig will delete the auto-generated kubeconfig, if it exists
-func MaybeDeleteConfig(ctl *api.ClusterConfig) {
-	p := AutoPath(ctl.ClusterName)
+func MaybeDeleteConfig(lc *api.ClusterLocator) {
+	p := AutoPath(lc.Name)
 
 	autoConfExists, err := utils.FileExists(p)
 	if err != nil {
@@ -166,7 +162,7 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 		return
 	}
 	if autoConfExists {
-		if err = isValidConfig(p, ctl.ClusterName); err != nil {
+		if err = isValidConfig(p, lc.Name); err != nil {
 			logger.Debug(err.Error())
 			return
 		}
@@ -183,7 +179,7 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 		return
 	}
 
-	if !deleteClusterInfo(config, ctl) {
+	if !deleteClusterInfo(config, lc) {
 		return
 	}
 
@@ -197,9 +193,9 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 // deleteClusterInfo removes a cluster's information from the kubeconfig if the cluster name
 // provided by ctl matches a eksctl-created cluster in the kubeconfig
 // returns 'true' if the existing config has changes and 'false' otherwise
-func deleteClusterInfo(existing *clientcmdapi.Config, ctl *api.ClusterConfig) bool {
+func deleteClusterInfo(existing *clientcmdapi.Config, lc *api.ClusterLocator) bool {
 	isChanged := false
-	clusterName := getCompleteClusterName(ctl)
+	clusterName := lc.String()
 
 	if existing.Clusters[clusterName] != nil {
 		delete(existing.Clusters, clusterName)

coolguy741/weave-eksctl

pkg/cfn/builder/cluster.go
@@ -79,7 +79,7 @@ func (c *ClusterResourceSet) addResourcesForControlPlane(version string) {
 	}
 
 	c.newResource("ControlPlane", &gfn.AWSEKSCluster{
-		Name:               gfn.NewString(c.spec.ClusterName),
+		Name:               gfn.NewString(c.spec.Metadata.Name),
 		RoleArn:            gfn.MakeFnGetAttString("ServiceRole.Arn"),
 		Version:            gfn.NewString(version),
 		ResourcesVpcConfig: clusterVPC,

pkg/cfn/builder/nodegroup.go
@@ -33,7 +33,7 @@ func NewNodeGroupResourceSet(spec *api.ClusterConfig, clusterStackName string, i
 		rs:               newResourceSet(),
 		id:               id,
 		clusterStackName: clusterStackName,
-		nodeGroupName:    fmt.Sprintf("%s-%d", spec.ClusterName, id),
+		nodeGroupName:    fmt.Sprintf("%s-%d", spec.Metadata.Name, id),
 		clusterSpec:      spec,
 		spec:             spec.NodeGroups[id],
 	}
@@ -156,7 +156,7 @@ func (n *NodeGroupResourceSet) addResourcesForNodeGroup() error {
 			"VPCZoneIdentifier":       vpcZoneIdentifier,
 			"Tags": []map[string]interface{}{
 				{"Key": "Name", "Value": fmt.Sprintf("%s-Node", n.nodeGroupName), "PropagateAtLaunch": "true"},
-				{"Key": "kubernetes.io/cluster/" + n.clusterSpec.ClusterName, "Value": "owned", "PropagateAtLaunch": "true"},
+				{"Key": "kubernetes.io/cluster/" + n.clusterSpec.Metadata.Name, "Value": "owned", "PropagateAtLaunch": "true"},
 			},
 		},
 		UpdatePolicy: map[string]map[string]string{

pkg/cfn/manager/api.go
@@ -36,9 +36,10 @@ type ChangeSet = cloudformation.DescribeChangeSetOutput
 
 // StackCollection stores the CloudFormation stack information
 type StackCollection struct {
-	cfn  cloudformationiface.CloudFormationAPI
-	spec *api.ClusterConfig
-	tags []*cloudformation.Tag
+	cfn         cloudformationiface.CloudFormationAPI
+	waitTimeout time.Duration
+	spec        *api.ClusterConfig
+	tags        []*cloudformation.Tag
 }
 
 func newTag(key, value string) *cloudformation.Tag {
@@ -48,16 +49,17 @@ func newTag(key, value string) *cloudformation.Tag {
 // NewStackCollection create a stack manager for a single cluster
 func NewStackCollection(provider api.ClusterProvider, spec *api.ClusterConfig) *StackCollection {
 	tags := []*cloudformation.Tag{
-		newTag(ClusterNameTag, spec.ClusterName),
+		newTag(ClusterNameTag, spec.Metadata.Name),
 	}
-	for key, value := range spec.Tags {
+	for key, value := range spec.Metadata.Tags {
 		tags = append(tags, newTag(key, value))
 	}
 	logger.Debug("tags = %#v", tags)
 	return &StackCollection{
-		cfn:  provider.CloudFormation(),
-		spec: spec,
-		tags: tags,
+		cfn:         provider.CloudFormation(),
+		waitTimeout: provider.WaitTimeout(),
+		spec:        spec,
+		tags:        tags,
 	}
 }
 
@@ -203,7 +205,7 @@ func (c *StackCollection) DeleteStack(name string) (*Stack, error) {
 	}
 	i.StackId = s.StackId
 	for _, tag := range s.Tags {
-		if *tag.Key == ClusterNameTag && *tag.Value == c.spec.ClusterName {
+		if *tag.Key == ClusterNameTag && *tag.Value == c.spec.Metadata.Name {
 			input := &cloudformation.DeleteStackInput{
 				StackName: i.StackId,
 			}
@@ -217,7 +219,7 @@ func (c *StackCollection) DeleteStack(name string) (*Stack, error) {
 	}
 
 	return nil, fmt.Errorf("cannot delete stack %q as it doesn't bare our %q tag", *s.StackName,
-		fmt.Sprintf("%s:%s", ClusterNameTag, c.spec.ClusterName))
+		fmt.Sprintf("%s:%s", ClusterNameTag, c.spec.Metadata.Name))
 }
 
 // WaitDeleteStack kills a stack by name and waits for DELETED status

pkg/cfn/manager/cluster.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (c *StackCollection) makeClusterStackName() string {
-	return "eksctl-" + c.spec.ClusterName + "-cluster"
+	return "eksctl-" + c.spec.Metadata.Name + "-cluster"
 }
 
 // CreateCluster creates the cluster

pkg/ctl/get/cluster.go
@@ -15,6 +15,7 @@ import (
 var listAllRegions bool
 
 func getClusterCmd() *cobra.Command {
+	p := &api.ProviderConfig{}
 	cfg := api.NewClusterConfig()
 
 	cmd := &cobra.Command{
@@ -22,7 +23,7 @@ func getClusterCmd() *cobra.Command {
 		Short:   "Get cluster(s)",
 		Aliases: []string{"clusters"},
 		Run: func(_ *cobra.Command, args []string) {
-			if err := doGetCluster(cfg, ctl.GetNameArg(args)); err != nil {
+			if err := doGetCluster(p, cfg, ctl.GetNameArg(args)); err != nil {
 				logger.Critical("%s\n", err.Error())
 				os.Exit(1)
 			}
@@ -31,38 +32,39 @@ func getClusterCmd() *cobra.Command {
 
 	fs := cmd.Flags()
 
-	fs.StringVarP(&cfg.ClusterName, "name", "n", "", "EKS cluster name")
+	fs.StringVarP(&cfg.Metadata.Name, "name", "n", "", "EKS cluster name")
 	fs.BoolVarP(&listAllRegions, "all-regions", "A", false, "List clusters across all supported regions")
 	fs.IntVar(&chunkSize, "chunk-size", defaultChunkSize, "Return large lists in chunks rather than all at once. Pass 0 to disable.")
 
-	fs.StringVarP(&cfg.Region, "region", "r", "", "AWS region")
-	fs.StringVarP(&cfg.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
+	fs.StringVarP(&p.Region, "region", "r", "", "AWS region")
+	fs.StringVarP(&p.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
 
 	fs.StringVarP(&output, "output", "o", "table", "Specifies the output format. Choose from table,json,yaml. Defaults to table.")
+
 	return cmd
 }
 
-func doGetCluster(cfg *api.ClusterConfig, name string) error {
-	regionGiven := cfg.Region != "" // eks.New resets this field, so we need to check if it was set in the fist place
-	ctl := eks.New(cfg)
+func doGetCluster(p *api.ProviderConfig, cfg *api.ClusterConfig, name string) error {
+	regionGiven := cfg.Metadata.Region != "" // eks.New resets this field, so we need to check if it was set in the fist place
+	ctl := eks.New(p, cfg)
 
-	if !cfg.IsSupportedRegion() {
-		return fmt.Errorf("--region=%s is not supported - use one of: %s", cfg.Region, strings.Join(api.SupportedRegions(), ", "))
+	if !ctl.IsSupportedRegion() {
+		return fmt.Errorf("--region=%s is not supported - use one of: %s", cfg.Metadata.Region, strings.Join(api.SupportedRegions(), ", "))
 	}
 
 	if regionGiven && listAllRegions {
-		logger.Warning("--region=%s is ignored, as --all-regions is given", cfg.Region)
+		logger.Warning("--region=%s is ignored, as --all-regions is given", cfg.Metadata.Region)
 	}
 
-	if cfg.ClusterName != "" && name != "" {
-		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ClusterName, name)
+	if cfg.Metadata.Name != "" && name != "" {
+		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.Metadata.Name, name)
 	}
 
 	if name != "" {
-		cfg.ClusterName = name
+		cfg.Metadata.Name = name
 	}
 
-	if cfg.ClusterName != "" && listAllRegions {
+	if cfg.Metadata.Name != "" && listAllRegions {
 		return fmt.Errorf("--all-regions is for listing all clusters, it must be used without cluster name flag/argument")
 	}
 
@@ -70,5 +72,5 @@ func doGetCluster(cfg *api.ClusterConfig, name string) error {
 		return err
 	}
 
-	return ctl.ListClusters(chunkSize, output, listAllRegions)
+	return ctl.ListClusters(cfg.Metadata.Name, chunkSize, output, listAllRegions)
 }

pkg/ctl/utils/write_kubeconfig.go
@@ -21,13 +21,14 @@ var (
 )
 
 func writeKubeconfigCmd() *cobra.Command {
+	p := &api.ProviderConfig{}
 	cfg := api.NewClusterConfig()
 
 	cmd := &cobra.Command{
 		Use:   "write-kubeconfig",
 		Short: "Write kubeconfig file for a given cluster",
 		Run: func(_ *cobra.Command, args []string) {
-			if err := doWriteKubeconfigCmd(cfg, ctl.GetNameArg(args)); err != nil {
+			if err := doWriteKubeconfigCmd(p, cfg, ctl.GetNameArg(args)); err != nil {
 				logger.Critical("%s\n", err.Error())
 				os.Exit(1)
 			}
@@ -36,10 +37,10 @@ func writeKubeconfigCmd() *cobra.Command {
 
 	fs := cmd.Flags()
 
-	fs.StringVarP(&cfg.ClusterName, "name", "n", "", "EKS cluster name (required)")
+	fs.StringVarP(&cfg.Metadata.Name, "name", "n", "", "EKS cluster name (required)")
 
-	fs.StringVarP(&cfg.Region, "region", "r", "", "AWS region")
-	fs.StringVarP(&cfg.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
+	fs.StringVarP(&p.Region, "region", "r", "", "AWS region")
+	fs.StringVarP(&p.Profile, "profile", "p", "", "AWS credentials profile to use (overrides the AWS_PROFILE environment variable)")
 
 	fs.BoolVar(&utilsAutoKubeconfigPath, "auto-kubeconfig", false, fmt.Sprintf("save kubeconfig file by cluster name – %q", kubeconfig.AutoPath("<name>")))
 	fs.StringVar(&utilsKubeconfigOutputPath, "kubeconfig", kubeconfig.DefaultPath, "path to write kubeconfig")
@@ -48,22 +49,22 @@ func writeKubeconfigCmd() *cobra.Command {
 	return cmd
 }
 
-func doWriteKubeconfigCmd(cfg *api.ClusterConfig, name string) error {
-	ctl := eks.New(cfg)
+func doWriteKubeconfigCmd(p *api.ProviderConfig, cfg *api.ClusterConfig, name string) error {
+	ctl := eks.New(p, cfg)
 
 	if err := ctl.CheckAuth(); err != nil {
 		return err
 	}
 
-	if cfg.ClusterName != "" && name != "" {
-		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.ClusterName, name)
+	if cfg.Metadata.Name != "" && name != "" {
+		return fmt.Errorf("--name=%s and argument %s cannot be used at the same time", cfg.Metadata.Name, name)
 	}
 
 	if name != "" {
-		cfg.ClusterName = name
+		cfg.Metadata.Name = name
 	}
 
-	if cfg.ClusterName == "" {
+	if cfg.Metadata.Name == "" {
 		return fmt.Errorf("--name must be set")
 	}
 
@@ -71,21 +72,21 @@ func doWriteKubeconfigCmd(cfg *api.ClusterConfig, name string) error {
 		if utilsKubeconfigOutputPath != kubeconfig.DefaultPath {
 			return fmt.Errorf("--kubeconfig and --auto-kubeconfig cannot be used at the same time")
 		}
-		utilsKubeconfigOutputPath = kubeconfig.AutoPath(cfg.ClusterName)
+		utilsKubeconfigOutputPath = kubeconfig.AutoPath(cfg.Metadata.Name)
 	}
 
-	cluster, err := ctl.DescribeControlPlane()
+	cluster, err := ctl.DescribeControlPlane(cfg.Metadata)
 	if err != nil {
 		return err
 	}
 
 	logger.Debug("cluster = %#v", cluster)
 
-	if err = ctl.GetCredentials(*cluster); err != nil {
+	if err = ctl.GetCredentials(*cluster, cfg); err != nil {
 		return err
 	}
 
-	clientConfigBase, err := ctl.NewClientConfig()
+	clientConfigBase, err := ctl.NewClientConfig(cfg)
 	if err != nil {
 		return err
 	}

pkg/utils/kubeconfig/kubeconfig.go
@@ -31,7 +31,7 @@ const (
 // if certificateAuthorityPath is no empty, it is used instead of
 // embedded certificate-authority-data
 func New(spec *api.ClusterConfig, username, certificateAuthorityPath string) (*clientcmdapi.Config, string, string) {
-	clusterName := getCompleteClusterName(spec)
+	clusterName := spec.Metadata.String()
 	contextName := fmt.Sprintf("%s@%s", username, clusterName)
 
 	c := &clientcmdapi.Config{
@@ -66,7 +66,7 @@ func AppendAuthenticator(c *clientcmdapi.Config, spec *api.ClusterConfig, comman
 	c.AuthInfos[c.CurrentContext].Exec = &clientcmdapi.ExecConfig{
 		APIVersion: "client.authentication.k8s.io/v1alpha1",
 		Command:    command,
-		Args:       []string{"token", "-i", spec.ClusterName},
+		Args:       []string{"token", "-i", spec.Metadata.Name},
 		/*
 			Args:       []string{"token", "-i", c.Cluster.ClusterName, "-r", c.roleARN},
 		*/
@@ -98,10 +98,6 @@ func Write(path string, newConfig *clientcmdapi.Config, setContext bool) (string
 	}
 
 	return configAccess.GetDefaultFilename(), nil
-}
-
-func getCompleteClusterName(spec *api.ClusterConfig) string {
-	return fmt.Sprintf("%s.%s.eksctl.io", spec.ClusterName, spec.Region)
 }
 
 func getConfigAccess(explicitPath string) clientcmd.ConfigAccess {
@@ -157,8 +153,8 @@ func isValidConfig(p, name string) error {
 }
 
 // MaybeDeleteConfig will delete the auto-generated kubeconfig, if it exists
-func MaybeDeleteConfig(ctl *api.ClusterConfig) {
-	p := AutoPath(ctl.ClusterName)
+func MaybeDeleteConfig(cl *api.ClusterMeta) {
+	p := AutoPath(cl.Name)
 
 	autoConfExists, err := utils.FileExists(p)
 	if err != nil {
@@ -166,7 +162,7 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 		return
 	}
 	if autoConfExists {
-		if err = isValidConfig(p, ctl.ClusterName); err != nil {
+		if err = isValidConfig(p, cl.Name); err != nil {
 			logger.Debug(err.Error())
 			return
 		}
@@ -183,7 +179,7 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 		return
 	}
 
-	if !deleteClusterInfo(config, ctl) {
+	if !deleteClusterInfo(config, cl) {
 		return
 	}
 
@@ -197,9 +193,9 @@ func MaybeDeleteConfig(ctl *api.ClusterConfig) {
 // deleteClusterInfo removes a cluster's information from the kubeconfig if the cluster name
 // provided by ctl matches a eksctl-created cluster in the kubeconfig
 // returns 'true' if the existing config has changes and 'false' otherwise
-func deleteClusterInfo(existing *clientcmdapi.Config, ctl *api.ClusterConfig) bool {
+func deleteClusterInfo(existing *clientcmdapi.Config, cl *api.ClusterMeta) bool {
 	isChanged := false
-	clusterName := getCompleteClusterName(ctl)
+	clusterName := cl.String()
 
 	if existing.Clusters[clusterName] != nil {
 		delete(existing.Clusters, clusterName)