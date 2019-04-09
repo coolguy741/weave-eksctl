unguiculus/eksctl

Rework stack management
- more visibility into what task manager does
  - print clear descriptions of what is being done
  - enable addition of `--dry-run` to every command
- cleaner main functions for create and delete commands
- more general abstraction for tasks
- expose all common operations through functions
- remove special purpose taks handler functions

pkg/cfn/manager/create_tasks.go
@@ -1,0 +1,49 @@
+package manager
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// CreateTasksForClusterWithNodeGroups defines all tasks required to create a cluster along
+// with some nodegroups; see CreateAllNodeGroups for how onlyNodeGroupSubset works
+func (c *StackCollection) CreateTasksForClusterWithNodeGroups(onlyNodeGroupSubset sets.String) *TaskSet {
+	tasks := &TaskSet{Parallel: false}
+
+	tasks.Append(
+		&taskWithoutParams{
+			info: fmt.Sprintf("create cluster control plane %q", c.spec.Metadata.Name),
+			call: c.createClusterTask,
+		},
+	)
+
+	nodeGroupTasks := c.CreateTasksForNodeGroups(onlyNodeGroupSubset)
+	if nodeGroupTasks.Len() > 0 {
+		nodeGroupTasks.Sub = true
+		tasks.Append(nodeGroupTasks)
+	}
+
+	return tasks
+}
+
+// CreateTasksForNodeGroups defines tasks required to create all of the nodegroups if
+// onlySubset is nil, otherwise just the tasks for nodegroups that are in onlySubset
+// will be defined
+func (c *StackCollection) CreateTasksForNodeGroups(onlySubset sets.String) *TaskSet {
+	tasks := &TaskSet{Parallel: true}
+
+	for i := range c.spec.NodeGroups {
+		ng := c.spec.NodeGroups[i]
+		if onlySubset != nil && !onlySubset.Has(ng.Name) {
+			continue
+		}
+		tasks.Append(&taskWithNodeGroupSpec{
+			info:      fmt.Sprintf("create nodegroup %q", ng.Name),
+			nodeGroup: ng,
+			call:      c.createNodeGroupTask,
+		})
+	}
+
+	return tasks
+}

pkg/cfn/manager/delete_tasks.go
@@ -1,0 +1,87 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// DeleteTasksForClusterWithNodeGroups defines tasks required to delete all the nodegroup
+// stacks and the cluster
+func (c *StackCollection) DeleteTasksForClusterWithNodeGroups(wait bool, cleanup func(chan error, string) error) (*TaskSet, error) {
+	tasks := &TaskSet{Parallel: false}
+
+	nodeGroupTasks, err := c.DeleteTasksForNodeGroups(nil, true, cleanup)
+	if err != nil {
+		return nil, err
+	}
+	if nodeGroupTasks.Len() > 0 {
+		nodeGroupTasks.Sub = true
+		tasks.Append(nodeGroupTasks)
+	}
+
+	clusterStack, err := c.DescribeClusterStack()
+	if err != nil {
+		return nil, err
+	}
+
+	info := fmt.Sprintf("delete cluster control plane %q", c.spec.Metadata.Name)
+	if wait {
+		tasks.Append(&taskWithStackSpec{
+			info:  info,
+			stack: clusterStack,
+			call:  c.WaitDeleteStackBySpec,
+		})
+	} else {
+		tasks.Append(&asyncTaskWithStackSpec{
+			info:  info,
+			stack: clusterStack,
+			call:  c.DeleteStackBySpec,
+		})
+	}
+
+	return tasks, nil
+}
+
+// DeleteTasksForNodeGroups defines tasks required to delete all of the nodegroups if
+// onlySubset is nil, otherwise just the tasks for nodegroups that are in onlySubset
+// will be defined
+func (c *StackCollection) DeleteTasksForNodeGroups(onlySubset sets.String, wait bool, cleanup func(chan error, string) error) (*TaskSet, error) {
+	nodeGroupStacks, err := c.DescribeNodeGroupStacks()
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := &TaskSet{Parallel: true}
+
+	for _, s := range nodeGroupStacks {
+		name := getNodeGroupName(s)
+		if onlySubset != nil && !onlySubset.Has(name) {
+			continue
+		}
+		if *s.StackStatus == cloudformation.StackStatusDeleteFailed && cleanup != nil {
+			tasks.Append(&taskWithNameParam{
+				info: fmt.Sprintf("cleanup for nodegroup %q", name),
+				call: cleanup,
+			})
+		}
+		info := fmt.Sprintf("delete nodegroup %q", name)
+		if wait {
+			tasks.Append(&taskWithStackSpec{
+				info:  info,
+				stack: s,
+				call:  c.WaitDeleteStackBySpec,
+			})
+		} else {
+			tasks.Append(&asyncTaskWithStackSpec{
+				info:  info,
+				stack: s,
+				call:  c.DeleteStackBySpec,
+			})
+		}
+	}
+
+	return tasks, nil
+}

pkg/cfn/manager/deprecated.go
@@ -1,57 +1,98 @@
 package manager
 
-// DeprecatedDeleteStackVPC deletes the VPC stack
-func (c *StackCollection) DeprecatedDeleteStackVPC(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-VPC"
+import (
+	"fmt"
+	"strings"
 
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/pkg/errors"
+	"github.com/weaveworks/eksctl/pkg/utils/waiters"
+)
+
+func deprecastedStackSuffices() []string {
+	return []string{
+		"DefaultNodeGroup",
+		"ControlPlane",
+		"ServiceRole",
+		"VPC",
 	}
-
-	return err
+}
+func fmtDeprecatedStacksRegexForCluster(name string) string {
+	const ourStackRegexFmt = "^EKS-%s-(%s)$"
+	return fmt.Sprintf(ourStackRegexFmt, name, strings.Join(deprecastedStackSuffices(), "|"))
 }
 
-// DeprecatedDeleteStackServiceRole deletes the service role stack
-func (c *StackCollection) DeprecatedDeleteStackServiceRole(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-ServiceRole"
-
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+// DeleteTasksForDeprecatedStacks all deprecated stacks
+func (c *StackCollection) DeleteTasksForDeprecatedStacks() (*TaskSet, error) {
+	stacks, err := c.ListStacks(fmtDeprecatedStacksRegexForCluster(c.spec.Metadata.Name))
+	if err != nil {
+		return nil, errors.Wrapf(err, "describing deprecated CloudFormation stacks for %q", c.spec.Metadata.Name)
+	}
+	if len(stacks) == 0 {
+		return nil, nil
 	}
 
-	return err
-}
+	deleteControlPlaneTask := &taskWithoutParams{
+		info: fmt.Sprintf("delete control plane %q", c.spec.Metadata.Name),
+		call: func(errs chan error) error {
+			_, err := c.provider.EKS().DescribeCluster(&eks.DescribeClusterInput{
+				Name: &c.spec.Metadata.Name,
+			})
+			if err != nil {
+				return err
+			}
 
-// DeprecatedDeleteStackDefaultNodeGroup deletes the default node group stack
-func (c *StackCollection) DeprecatedDeleteStackDefaultNodeGroup(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-DefaultNodeGroup"
+			_, err = c.provider.EKS().DeleteCluster(&eks.DeleteClusterInput{
+				Name: &c.spec.Metadata.Name,
+			})
+			if err != nil {
+				return err
+			}
 
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+			newRequest := func() *request.Request {
+				input := &eks.DescribeClusterInput{
+					Name: &c.spec.Metadata.Name,
+				}
+				req, _ := c.provider.EKS().DescribeClusterRequest(input)
+				return req
+			}
+
+			msg := fmt.Sprintf("waiting for control plane %q to be deleted", c.spec.Metadata.Name)
+
+			acceptors := waiters.MakeAcceptors(
+				"Cluster.Status",
+				eks.ClusterStatusDeleting,
+				[]string{
+					eks.ClusterStatusFailed,
+				},
+			)
+
+			return waiters.Wait(c.spec.Metadata.Name, msg, acceptors, newRequest, c.provider.WaitTimeout(), nil)
+		},
 	}
 
-	return err
-}
-
-// DeprecatedDeleteStackControlPlane deletes the control plane stack
-func (c *StackCollection) DeprecatedDeleteStackControlPlane(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-ControlPlane"
-
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+	cpStackFound := false
+	for _, s := range stacks {
+		if strings.HasSuffix(*s.StackName, "-ControlPlane") {
+			cpStackFound = true
+		}
 	}
+	tasks := &TaskSet{}
 
-	return err
+	for _, suffix := range deprecastedStackSuffices() {
+		for _, s := range stacks {
+			if strings.HasSuffix(*s.StackName, "-"+suffix) {
+				if suffix == "-ControlPlane" && !cpStackFound {
+					tasks.Append(deleteControlPlaneTask)
+				} else {
+					tasks.Append(&taskWithStackSpec{
+						stack: s,
+						call:  c.WaitDeleteStackBySpec,
+					})
+				}
+			}
+		}
+	}
+	return tasks, nil
 }

coolguy741/weave-eksctl

pkg/cfn/manager/create_tasks.go
@@ -1,0 +1,49 @@
+package manager
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// CreateTasksForClusterWithNodeGroups defines all tasks required to create a cluster along
+// with some nodegroups; see CreateAllNodeGroups for how onlyNodeGroupSubset works
+func (c *StackCollection) CreateTasksForClusterWithNodeGroups(onlyNodeGroupSubset sets.String) *TaskTree {
+	tasks := &TaskTree{Parallel: false}
+
+	tasks.Append(
+		&taskWithoutParams{
+			info: fmt.Sprintf("create cluster control plane %q", c.spec.Metadata.Name),
+			call: c.createClusterTask,
+		},
+	)
+
+	nodeGroupTasks := c.CreateTasksForNodeGroups(onlyNodeGroupSubset)
+	if nodeGroupTasks.Len() > 0 {
+		nodeGroupTasks.Sub = true
+		tasks.Append(nodeGroupTasks)
+	}
+
+	return tasks
+}
+
+// CreateTasksForNodeGroups defines tasks required to create all of the nodegroups if
+// onlySubset is nil, otherwise just the tasks for nodegroups that are in onlySubset
+// will be defined
+func (c *StackCollection) CreateTasksForNodeGroups(onlySubset sets.String) *TaskTree {
+	tasks := &TaskTree{Parallel: true}
+
+	for i := range c.spec.NodeGroups {
+		ng := c.spec.NodeGroups[i]
+		if onlySubset != nil && !onlySubset.Has(ng.Name) {
+			continue
+		}
+		tasks.Append(&taskWithNodeGroupSpec{
+			info:      fmt.Sprintf("create nodegroup %q", ng.Name),
+			nodeGroup: ng,
+			call:      c.createNodeGroupTask,
+		})
+	}
+
+	return tasks
+}

pkg/cfn/manager/delete_tasks.go
@@ -1,0 +1,87 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// DeleteTasksForClusterWithNodeGroups defines tasks required to delete all the nodegroup
+// stacks and the cluster
+func (c *StackCollection) DeleteTasksForClusterWithNodeGroups(wait bool, cleanup func(chan error, string) error) (*TaskTree, error) {
+	tasks := &TaskTree{Parallel: false}
+
+	nodeGroupTasks, err := c.DeleteTasksForNodeGroups(nil, true, cleanup)
+	if err != nil {
+		return nil, err
+	}
+	if nodeGroupTasks.Len() > 0 {
+		nodeGroupTasks.Sub = true
+		tasks.Append(nodeGroupTasks)
+	}
+
+	clusterStack, err := c.DescribeClusterStack()
+	if err != nil {
+		return nil, err
+	}
+
+	info := fmt.Sprintf("delete cluster control plane %q", c.spec.Metadata.Name)
+	if wait {
+		tasks.Append(&taskWithStackSpec{
+			info:  info,
+			stack: clusterStack,
+			call:  c.WaitDeleteStackBySpec,
+		})
+	} else {
+		tasks.Append(&asyncTaskWithStackSpec{
+			info:  info,
+			stack: clusterStack,
+			call:  c.DeleteStackBySpec,
+		})
+	}
+
+	return tasks, nil
+}
+
+// DeleteTasksForNodeGroups defines tasks required to delete all of the nodegroups if
+// onlySubset is nil, otherwise just the tasks for nodegroups that are in onlySubset
+// will be defined
+func (c *StackCollection) DeleteTasksForNodeGroups(onlySubset sets.String, wait bool, cleanup func(chan error, string) error) (*TaskTree, error) {
+	nodeGroupStacks, err := c.DescribeNodeGroupStacks()
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := &TaskTree{Parallel: true}
+
+	for _, s := range nodeGroupStacks {
+		name := getNodeGroupName(s)
+		if onlySubset != nil && !onlySubset.Has(name) {
+			continue
+		}
+		if *s.StackStatus == cloudformation.StackStatusDeleteFailed && cleanup != nil {
+			tasks.Append(&taskWithNameParam{
+				info: fmt.Sprintf("cleanup for nodegroup %q", name),
+				call: cleanup,
+			})
+		}
+		info := fmt.Sprintf("delete nodegroup %q", name)
+		if wait {
+			tasks.Append(&taskWithStackSpec{
+				info:  info,
+				stack: s,
+				call:  c.WaitDeleteStackBySpec,
+			})
+		} else {
+			tasks.Append(&asyncTaskWithStackSpec{
+				info:  info,
+				stack: s,
+				call:  c.DeleteStackBySpec,
+			})
+		}
+	}
+
+	return tasks, nil
+}

pkg/cfn/manager/deprecated.go
@@ -1,57 +1,98 @@
 package manager
 
-// DeprecatedDeleteStackVPC deletes the VPC stack
-func (c *StackCollection) DeprecatedDeleteStackVPC(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-VPC"
+import (
+	"fmt"
+	"strings"
 
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/pkg/errors"
+	"github.com/weaveworks/eksctl/pkg/utils/waiters"
+)
+
+func deprecatedStackSuffices() []string {
+	return []string{
+		"DefaultNodeGroup",
+		"ControlPlane",
+		"ServiceRole",
+		"VPC",
 	}
-
-	return err
+}
+func fmtDeprecatedStacksRegexForCluster(name string) string {
+	const ourStackRegexFmt = "^EKS-%s-(%s)$"
+	return fmt.Sprintf(ourStackRegexFmt, name, strings.Join(deprecatedStackSuffices(), "|"))
 }
 
-// DeprecatedDeleteStackServiceRole deletes the service role stack
-func (c *StackCollection) DeprecatedDeleteStackServiceRole(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-ServiceRole"
-
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+// DeleteTasksForDeprecatedStacks all deprecated stacks
+func (c *StackCollection) DeleteTasksForDeprecatedStacks() (*TaskTree, error) {
+	stacks, err := c.ListStacks(fmtDeprecatedStacksRegexForCluster(c.spec.Metadata.Name))
+	if err != nil {
+		return nil, errors.Wrapf(err, "describing deprecated CloudFormation stacks for %q", c.spec.Metadata.Name)
+	}
+	if len(stacks) == 0 {
+		return nil, nil
 	}
 
-	return err
-}
+	deleteControlPlaneTask := &taskWithoutParams{
+		info: fmt.Sprintf("delete control plane %q", c.spec.Metadata.Name),
+		call: func(errs chan error) error {
+			_, err := c.provider.EKS().DescribeCluster(&eks.DescribeClusterInput{
+				Name: &c.spec.Metadata.Name,
+			})
+			if err != nil {
+				return err
+			}
 
-// DeprecatedDeleteStackDefaultNodeGroup deletes the default node group stack
-func (c *StackCollection) DeprecatedDeleteStackDefaultNodeGroup(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-DefaultNodeGroup"
+			_, err = c.provider.EKS().DeleteCluster(&eks.DeleteClusterInput{
+				Name: &c.spec.Metadata.Name,
+			})
+			if err != nil {
+				return err
+			}
 
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+			newRequest := func() *request.Request {
+				input := &eks.DescribeClusterInput{
+					Name: &c.spec.Metadata.Name,
+				}
+				req, _ := c.provider.EKS().DescribeClusterRequest(input)
+				return req
+			}
+
+			msg := fmt.Sprintf("waiting for control plane %q to be deleted", c.spec.Metadata.Name)
+
+			acceptors := waiters.MakeAcceptors(
+				"Cluster.Status",
+				eks.ClusterStatusDeleting,
+				[]string{
+					eks.ClusterStatusFailed,
+				},
+			)
+
+			return waiters.Wait(c.spec.Metadata.Name, msg, acceptors, newRequest, c.provider.WaitTimeout(), nil)
+		},
 	}
 
-	return err
-}
-
-// DeprecatedDeleteStackControlPlane deletes the control plane stack
-func (c *StackCollection) DeprecatedDeleteStackControlPlane(wait bool) error {
-	var err error
-	stackName := "EKS-" + c.spec.Metadata.Name + "-ControlPlane"
-
-	if wait {
-		err = c.BlockingWaitDeleteStack(stackName, true)
-	} else {
-		_, err = c.DeleteStack(stackName, true)
+	cpStackFound := false
+	for _, s := range stacks {
+		if strings.HasSuffix(*s.StackName, "-ControlPlane") {
+			cpStackFound = true
+		}
 	}
+	tasks := &TaskTree{}
 
-	return err
+	for _, suffix := range deprecatedStackSuffices() {
+		for _, s := range stacks {
+			if strings.HasSuffix(*s.StackName, "-"+suffix) {
+				if suffix == "-ControlPlane" && !cpStackFound {
+					tasks.Append(deleteControlPlaneTask)
+				} else {
+					tasks.Append(&taskWithStackSpec{
+						stack: s,
+						call:  c.WaitDeleteStackBySpec,
+					})
+				}
+			}
+		}
+	}
+	return tasks, nil
 }