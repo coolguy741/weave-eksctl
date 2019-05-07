manabusakai/eksctl

Update assets for aws-node

pkg/addons/default/assets.go
@@ -1,5 +1,6 @@
 // Code generated by go-bindata.
 // sources:
+// assets/aws-node-1.10.yaml
 // assets/aws-node.yaml
 // assets/coredns.yaml
 // DO NOT EDIT!
@@ -69,7 +70,27 @@ func (fi bindataFileInfo) Sys() interface{} {
 	return nil
 }
 
-var _awsNodeYaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdd\x6e\xe3\x36\x13\xbd\xd7\x53\x0c\xfc\x5d\x2c\xf0\xa1\x92\xe3\xec\x36\x4d\x75\xe7\x75\xdc\x74\xd1\xc4\x6b\xc4\xdd\x2c\x8a\x45\x60\xd0\xd4\x44\x66\x4d\x91\x2c\x39\x94\xe3\x7d\xfa\x82\x92\x7f\xa4\x58\xf6\xb6\x45\x81\xf2\xc6\x32\x67\xe6\x0c\xe7\xf0\x90\xc3\x38\x8e\x23\x66\xc4\x23\x5a\x27\xb4\x4a\xc1\x2e\x18\x4f\x98\xa7\xa5\xb6\xe2\x2b\x23\xa1\x55\xb2\xba\x76\x89\xd0\xfd\x72\x10\xfd\x0f\x56\x7e\x81\x56\x21\xa1\x83\xb2\x0e\x71\xb0\xc0\x67\x6d\x11\x06\xc9\x75\x72\x01\x6e\xa9\xbd\xcc\xc0\x3b\x3c\x0b\xb5\x40\x62\x83\x68\x25\x54\x96\xc2\x48\x7a\x47\x68\x1f\xb4\xc4\xa8\x40\x62\x19\x23\x96\x46\x00\x8a\x15\x98\x02\x5b\xbb\x58\xe9\x0c\x23\xeb\x25\xba\x34\x8a\x81\x19\x71\x6b\xb5\x37\x2e\x38\xc5\xc0\x6d\x56\xe1\xb2\x82\x7d\xd5\x8a\xad\x5d\xc2\x75\x11\x01\x58\x74\xda\x5b\x8e\x5b\xb7\xde\xff\x7b\xd5\x6f\x40\x75\x86\x71\x74\x11\x84\x1a\x16\x0d\x7b\x13\x1b\xbe\xf4\x7a\x4f\xc7\x30\x46\x67\xae\xc6\xd1\x19\xba\x53\x88\xf0\xa5\x27\x85\xa3\xde\x77\xd0\x5b\x33\xe2\xcb\xf0\x91\x23\xf5\x9e\x5e\xa7\xc0\x17\x42\x55\xd1\xd8\x95\x2c\x63\x58\x68\xe5\x90\xce\x20\x3f\x45\xaf\xb7\xb0\xdc\x11\x3b\x43\x5b\x0a\x8e\x43\xce\xb5\x57\x74\x8e\x5b\x38\x14\x91\x56\x7b\x1c\xbb\x8d\x23\x2c\x8e\xb0\xff\x5b\x79\xbc\x17\x2a\x13\x2a\x3f\xab\x12\x2d\xf1\x01\x9f\x83\x65\x47\xf4\x99\x55\x47\x00\xc7\x1a\x3c\xc2\x74\x7e\xf1\x3b\x72\xaa\xc4\xd7\xc9\xec\xdf\xe3\xb3\x86\xb8\xa9\xf6\x76\x86\xd4\xe2\x97\x19\xe3\xfe\x02\x95\x3f\xb6\xa9\x3c\xa8\x68\xcf\xdd\x3f\xd9\x6c\x00\xc9\x16\x28\x2b\xf1\x01\xac\xae\x5d\xcc\x8c\x69\xf2\x60\x90\x07\x9b\x37\x19\x23\x9c\x91\x65\x84\xf9\xa6\xf6\xa6\x8d\xc1\x14\x1e\xb4\x94\x42\xe5\x9f\x2a\x87\x08\xc0\xa1\x44\x4e\xda\xd6\x3e\x45\x10\xec\x5d\x23\x45\x57\x12\x00\xc2\xc2\x48\x46\xb8\x0d\x6a\x14\x12\x86\x6c\xc5\x77\x23\x84\xc1\x94\xd2\x54\xed\x75\xc3\xd9\xf1\x25\x66\x5e\xa2\x4d\x98\x34\x4b\x96\x1c\x48\x0e\xba\xe3\x56\x90\xe0\x4c\xc6\x46\x67\x29\xbc\x79\x53\x85\xed\x8a\xae\xbe\x5b\xdb\x3e\x79\x4d\x6b\x18\x4b\xed\x68\x82\xb4\xd6\x76\x95\x02\x59\xbf\x9b\x27\x2d\xd1\xb6\x97\x13\x83\x36\x61\x4e\xdb\x14\xc6\x2f\xc2\x55\xa7\x3c\x0c\xae\x15\x31\xa1\xd0\x36\x5c\x45\xc1\x72\x4c\xe1\xea\xe2\xf2\xdd\xc5\x60\xf0\xee\xed\xbb\xef\x2f\x93\x6c\x65\x13\xe4\x36\xf1\x2e\x5e\xa3\xa3\xf8\xb2\x7d\x07\xf6\xeb\x7f\x71\x60\x88\x2b\x91\x96\x83\xe4\x6d\x72\xb9\xe7\xa2\x42\x9c\x7a\x29\xa7\x5a\x0a\xbe\x49\x61\x28\xd7\x6c\xe3\xf6\x76\xa3\x2d\x35\xa8\x8b\x0f\xcb\x9a\x6a\x4b\x29\x5c\x0d\xae\x7e\xb8\xde\x9b\x77\x2a\x2b\x90\xac\xe0\x07\x94\x23\xed\xd5\x03\x55\x99\x36\x62\xe3\xad\xdf\xf0\xf3\x6c\xfe\x38\x1d\xcd\x7f\xb9\x9e\xcd\x47\x93\x0f\xf3\xbb\x8f\xb7\x77\xe3\xc7\xf1\x5d\xc3\x15\xa0\x64\xd2\x63\x0a\x37\xe3\xf7\x9f\x6e\x3b\x30\xee\x7f\x9b\x4f\x3e\xde\x8c\xe7\x93\xe1\xfd\xf8\x38\xee\x27\xab\x8b\xb4\x35\x0d\xf0\x2c\x50\x66\xdb\x4b\xa3\xc3\x32\x65\xb4\x4c\x2b\x1d\x24\xa1\x86\xb0\xed\x1d\x69\x3f\x0f\x7f\x1d\xfd\x5c\x25\x9d\x4d\x87\xa3\x7f\x33\xf3\xee\x04\x24\xfb\x63\xbb\xf7\x6e\xf5\x8b\xc3\xe4\x1f\x1e\x1d\xb9\x36\x28\x37\x3e\x85\xc1\x45\x71\x38\x0b\xc8\xbd\x15\xb4\x19\x69\x45\xf8\x42\x4d\x6f\x63\x45\x29\x24\xe6\x98\xb5\x34\x0c\x50\x6a\xe9\x0b\xbc\x0f\xea\x6f\x49\xa3\x08\x33\xf5\x6a\xfb\xe1\x04\xf4\xb5\xa1\x3e\x57\xa2\xbf\x10\xea\x48\x22\x5c\x89\x78\x21\x54\x9c\x09\x7b\x0e\x02\x89\x57\x10\x0a\x29\xc9\x3a\x41\x14\xd2\xb7\x40\x4a\x66\xfb\x52\xe7\x47\xe1\x52\xe7\x67\x42\x43\x94\xf5\xaa\x9f\x69\xbe\x42\x9b\x38\xcd\x57\x47\x08\xb5\xad\x61\xaa\xb9\x69\x1c\xd9\xd3\xd5\x86\xa5\x55\xa9\x9a\x9c\xd7\xa9\x8f\x89\x8b\xcf\x54\x7c\x06\xa8\x8b\xbe\xf8\x44\xf5\x67\x60\xda\x04\xc6\xa7\x8a\xff\x26\xc6\x6b\x3a\x5f\x3f\x2c\x98\x11\x87\x2e\x76\xe2\x21\xe0\x1d\xe9\xe2\x61\x2b\xf9\x1b\x7c\x16\x4a\x84\x0b\xb5\xa3\xd7\xa1\x12\x5c\xab\x67\x91\xbb\xa4\xfb\x79\xb8\xbb\xd5\x1d\xd7\xa1\x6f\x6d\xdb\x7f\x04\x90\xd7\x2f\x86\x53\x8f\xca\xf2\xf0\xc8\xaa\x3a\xc8\x60\xd7\x4d\xeb\xb2\x8d\xf4\x96\xc9\x66\xfe\xba\x87\x08\x95\x7b\xc9\x6c\xc3\x50\xb7\xd8\xaa\xae\xf1\xe4\xc3\xa8\x9e\x8b\xa2\x3f\x03\x00\x00\xff\xff\xa7\x2d\x3d\xa5\x8f\x0b\x00\x00")
+var _awsNode110Yaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdf\x6f\xdb\x36\x10\x7e\xd7\x5f\x71\x70\x1f\x0a\x0c\x95\x6c\x67\x59\x96\xe9\xcd\x75\xbc\xac\x58\xe2\x1a\xf1\x9a\x62\x28\x02\x83\xa6\x2e\x32\x67\x8a\xe4\xc8\xa3\x1d\xf7\xaf\x1f\x28\xf9\x87\x14\xcb\xee\x36\x0c\x18\x5f\x2c\xf3\xee\xbe\xe3\x7d\xfc\xc8\x63\x1c\xc7\x11\x33\xe2\x11\xad\x13\x5a\xa5\x60\xe7\x8c\x27\xcc\xd3\x42\x5b\xf1\x95\x91\xd0\x2a\x59\x5e\xbb\x44\xe8\xee\xaa\x1f\xbd\x81\xa5\x9f\xa3\x55\x48\xe8\x60\x55\x85\x38\x98\xe3\xb3\xb6\x08\xfd\xe4\x3a\xe9\x81\x5b\x68\x2f\x33\xf0\x0e\xcf\x42\xcd\x91\x58\x3f\x5a\x0a\x95\xa5\x30\x94\xde\x11\xda\x07\x2d\x31\x2a\x90\x58\xc6\x88\xa5\x11\x80\x62\x05\xa6\xc0\xd6\x2e\x56\x3a\xc3\xc8\x7a\x89\x2e\x8d\x62\x60\x46\xdc\x5a\xed\x8d\x0b\x4e\x31\x70\x9b\x95\xb8\xac\x60\x5f\xb5\x62\x6b\x97\x70\x5d\x44\x00\x16\x9d\xf6\x96\xe3\xd6\xad\xf3\x5d\xa7\xfc\x0d\xa8\xce\x30\x8e\x2e\x82\x50\xc3\xbc\x66\xaf\x63\xc3\x97\x4e\xe7\xe9\x18\xc6\xe8\xcc\x55\x38\x3a\x43\x77\x0a\x11\xbe\x74\xa4\x70\xd4\x79\x07\x9d\x35\x23\xbe\x08\x1f\x39\x52\xe7\xe9\x75\x0a\x7c\x21\x54\x25\x8d\x6d\xc9\x32\x86\x85\x56\x0e\xe9\x0c\xf2\x53\xf4\x7a\x0b\x57\x3b\x62\xa7\x68\x57\x82\xe3\x80\x73\xed\x15\x9d\xe3\x16\x0e\x45\xa4\xe5\x1e\xc7\x6e\xe3\x08\x8b\x23\xec\xff\x57\x1e\xef\x85\xca\x84\xca\xcf\xaa\x44\x4b\x7c\xc0\xe7\x60\xd9\x11\x7d\x66\xd5\x11\xc0\xb1\x06\x8f\x30\x9d\x9f\xff\x81\x9c\x4a\xf1\xb5\x32\xfb\xcf\xf8\xac\x20\x6e\xca\xbd\x9d\x22\x35\xf8\x65\xc6\xb8\xbf\x41\xe5\x4f\x4d\x2a\x0f\x2a\xda\x73\xf7\x6f\x36\x1b\x40\xb2\x39\xca\x52\x7c\x00\xcb\x6b\x17\x33\x63\xea\x3c\x18\xe4\xc1\xe6\x4d\xc6\x08\xa7\x64\x19\x61\xbe\xa9\xbc\x69\x63\x30\x85\x07\x2d\xa5\x50\xf9\xa7\xd2\x21\x02\x70\x28\x91\x93\xb6\x95\x4f\x11\x04\x7b\x57\x4b\xd1\x96\x04\x80\xb0\x30\x92\x11\x6e\x83\x6a\x85\x84\x21\x1b\xf1\xed\x08\x61\x30\xa5\x34\x95\x7b\x5d\x73\x76\x7c\x81\x99\x97\x68\x13\x26\xcd\x82\x25\x07\x92\x83\xee\xb8\x15\x24\x38\x93\xb1\xd1\x59\x0a\x6f\xdf\x96\x61\xbb\xa2\xcb\xef\xc6\xb6\x8f\x5f\xd3\x1a\xc6\x42\x3b\x1a\x23\xad\xb5\x5d\xa6\x40\xd6\xef\xe6\x49\x4b\xb4\xcd\xe5\xc4\xa0\x4d\x98\xd3\x36\x85\xd1\x8b\x70\xe5\x29\x0f\x83\x6b\x45\x4c\x28\xb4\x35\x57\x51\xb0\x1c\x53\xb8\xea\x5d\x5c\xf6\xfa\xfd\xcb\xef\x2f\x7f\xb8\x48\xb2\xa5\x4d\x90\xdb\xc4\xbb\x78\x8d\x8e\xe2\x8b\xe6\x1d\xd8\xad\xfe\xc5\x81\x21\xae\x44\xba\xea\x27\x97\x49\x6f\xcf\x45\x89\x38\xf1\x52\x4e\xb4\x14\x7c\x93\xc2\x40\xae\xd9\xc6\xed\xed\x46\x5b\xaa\x51\x17\x1f\x96\x35\xd1\x96\x52\xb8\xea\x5f\xfd\x78\xbd\x37\xef\x54\x56\x20\x59\xc1\x0f\x28\x47\xda\xab\x06\xaa\x55\x5a\x8b\x8d\xb7\x7e\x83\xcf\xd3\xd9\xe3\x64\x38\xfb\xf5\x7a\x3a\x1b\x8e\x3f\xcc\xee\x3e\xde\xde\x8d\x1e\x47\x77\x35\x57\x80\x15\x93\x1e\x53\xb8\x19\xbd\xff\x74\xdb\x82\x71\xff\xfb\x6c\xfc\xf1\x66\x34\x1b\x0f\xee\x47\xc7\x71\x3f\x5b\x5d\xa4\x8d\x69\x80\x67\x81\x32\xdb\x5e\x1a\x2d\x96\x09\xa3\x45\x5a\xea\x20\x09\x35\x84\x6d\x6f\x49\xfb\x79\xf0\xdb\xf0\x97\x32\xe9\x74\x32\x18\xfe\x97\x99\x77\x27\x20\xd9\x1f\xdb\xbd\x77\xa3\x5f\x1c\x26\xff\xf4\xe8\xc8\x35\x41\xb9\xf1\x29\xf4\x7b\xc5\xe1\x2c\x20\xf7\x56\xd0\x66\xa8\x15\xe1\x0b\xd5\xbd\x8d\x15\x2b\x21\x31\xc7\xac\xa1\x61\x80\x95\x96\xbe\xc0\xfb\xa0\xfe\x86\x34\x8a\x30\x53\xad\xb6\x1b\x4e\x40\x57\x1b\xea\x72\x25\xba\x73\xa1\x8e\x24\xc2\x95\x88\xe7\x42\xc5\x99\xb0\xe7\x20\x90\x78\x09\xa1\x90\x92\xac\x15\x44\x21\x7d\x0b\x64\xc5\x6c\x57\xea\xfc\x28\x5c\xea\xfc\x4c\x68\x88\xb2\x5e\x75\x33\xcd\x97\x68\x13\xa7\xf9\xf2\x08\xa1\xb2\xd5\x4c\x15\x37\xb5\x23\x7b\xba\xda\xb0\xb4\x32\x55\x9d\xf3\x2a\xf5\x31\x71\xf1\x99\x8a\xcf\x00\xb5\xd1\x17\x9f\xa8\xfe\x0c\x4c\x93\xc0\xf8\x54\xf1\xdf\xc4\x78\x4d\xe7\xeb\x87\x05\x33\xe2\xd0\xc5\x4e\x3c\x04\xbc\x23\x5d\x3c\x6c\x25\x7f\x83\xcf\x42\x89\x70\xa1\xb6\xf4\x3a\x54\x82\x6b\xf5\x2c\x72\x97\xb4\x3f\x0f\x77\xb7\xba\xe3\x3a\xf4\xad\x6d\xfb\x8f\x00\xf2\xea\xc5\x70\xea\x51\xf9\x06\xfa\x49\xbf\x17\x9a\xae\x83\xce\xb6\x2f\x77\xde\x01\x93\x12\x14\xae\xd1\x96\xed\x78\x67\x70\x9d\xea\xd9\xb6\x7b\x96\x95\x3d\xa7\xbf\xeb\xbf\x15\x51\x46\x7a\xcb\x64\x7d\xc5\x55\xd7\x11\x2a\xf7\x92\xd9\x9a\xa1\x6a\xca\x25\x13\xa3\xf1\x87\x61\x35\xf7\x57\x00\x00\x00\xff\xff\x41\x46\x08\xc0\xbf\x0b\x00\x00")
+
+func awsNode110YamlBytes() ([]byte, error) {
+	return bindataRead(
+		_awsNode110Yaml,
+		"aws-node-1.10.yaml",
+	)
+}
+
+func awsNode110Yaml() (*asset, error) {
+	bytes, err := awsNode110YamlBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "aws-node-1.10.yaml", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _awsNodeYaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdf\x6f\xdb\x36\x10\x7e\xd7\x5f\x71\xf0\x1e\x0a\x0c\x93\x1c\x77\x59\x97\xe9\xcd\x75\xbc\xae\x58\xe2\x1a\xf1\x9a\x62\x28\x02\x83\xa6\x2e\x32\x67\x8a\xe4\xc8\xa3\x1c\xf7\xaf\x1f\x28\xf9\x87\x64\xcb\xee\x36\x0c\x18\x5f\x2c\xf3\xee\xbe\xe3\x7d\xfc\xc8\x63\x1c\xc7\x11\x33\xe2\x11\xad\x13\x5a\xa5\x60\x17\x8c\x27\xcc\xd3\x52\x5b\xf1\x85\x91\xd0\x2a\x59\xdd\xb8\x44\xe8\x7e\x39\x88\xbe\x81\x95\x5f\xa0\x55\x48\xe8\xa0\xac\x43\x1c\x2c\xf0\x59\x5b\x84\x41\x72\x93\x5c\x81\x5b\x6a\x2f\x33\xf0\x0e\x2f\x42\x2d\x90\xd8\x20\x5a\x09\x95\xa5\x30\x92\xde\x11\xda\x07\x2d\x31\x2a\x90\x58\xc6\x88\xa5\x11\x80\x62\x05\xa6\xc0\xd6\x2e\x56\x3a\xc3\xc8\x7a\x89\x2e\x8d\x62\x60\x46\xbc\xb3\xda\x1b\x17\x9c\x62\xe0\x36\xab\x70\x59\xc1\xbe\x68\xc5\xd6\x2e\xe1\xba\x88\x00\x2c\x3a\xed\x2d\xc7\xad\x5b\xef\xdb\x5e\xf5\x1b\x50\x9d\x61\x1c\x5d\x04\xa1\x86\x45\xc3\xde\xc4\x86\xcf\xbd\xde\xd3\x29\x8c\xd1\x99\xab\x71\x74\x86\xee\x1c\x22\x7c\xee\x49\xe1\xa8\xf7\x1d\xf4\xd6\x8c\xf8\x32\x7c\xe4\x48\xbd\xa7\xe3\x14\xf8\x42\xa8\x2a\x1a\xbb\x92\x65\x0c\x0b\xad\x1c\xd2\x05\xe4\xa7\xe8\x78\x0b\xcb\x1d\xb1\x33\xb4\xa5\xe0\x38\xe4\x5c\x7b\x45\x97\xb8\x85\x43\x11\x69\xb5\xc7\xb1\xdb\x38\xc2\xe2\x04\xfb\xff\x95\xc7\x5b\xa1\x32\xa1\xf2\x8b\x2a\xd1\x12\x1f\xf0\x39\x58\x76\x44\x5f\x58\x75\x04\x70\xaa\xc1\x13\x4c\xe7\x17\x7f\x20\xa7\x4a\x7c\x9d\xcc\xfe\x33\x3e\x6b\x88\xdb\x6a\x6f\x67\x48\x2d\x7e\x99\x31\xee\x6f\x50\xf9\x53\x9b\xca\x83\x8a\xf6\xdc\xfd\x9b\xcd\x06\x90\x6c\x81\xb2\x12\x1f\xc0\xea\xc6\xc5\xcc\x98\x26\x0f\x06\x79\xb0\x79\x93\x31\xc2\x19\x59\x46\x98\x6f\x6a\x6f\xda\x18\x4c\xe1\x41\x4b\x29\x54\xfe\xb1\x72\x88\x00\x1c\x4a\xe4\xa4\x6d\xed\x53\x04\xc1\xde\x35\x52\x74\x25\x01\x20\x2c\x8c\x64\x84\xdb\xa0\x46\x21\x61\xc8\x56\x7c\x37\x42\x18\x4c\x29\x4d\xd5\x5e\x37\x9c\x1d\x5f\x62\xe6\x25\xda\x84\x49\xb3\x64\xc9\x81\xe4\xa0\x3b\x6e\x05\x09\xce\x64\x6c\x74\x96\xc2\xab\x57\x55\xd8\xae\xe8\xea\xbb\xb5\xed\x93\x63\x5a\xc3\x58\x6a\x47\x13\xa4\xb5\xb6\xab\x14\xc8\xfa\xdd\x3c\x69\x89\xb6\xbd\x9c\x18\xb4\x09\x73\xda\xa6\x30\x7e\x11\xae\x3a\xe5\x61\x70\xad\x88\x09\x85\xb6\xe1\x2a\x0a\x96\x63\x0a\x6f\xae\x5e\x5f\x5f\x0d\x06\xd7\xdf\x5f\xff\xf0\x3a\xc9\x56\x36\x41\x6e\x13\xef\xe2\x35\x3a\x8a\x5f\xb7\xef\xc0\x7e\xfd\x2f\x0e\x0c\x71\x25\xd2\x72\x90\x5c\x27\x57\x7b\x2e\x2a\xc4\xa9\x97\x72\xaa\xa5\xe0\x9b\x14\x86\x72\xcd\x36\x6e\x6f\x37\xda\x52\x83\xba\xf8\xb0\xac\xa9\xb6\x94\xc2\x9b\xc1\x9b\x1f\x6f\xf6\xe6\x9d\xca\x0a\x24\x2b\xf8\x01\xe5\x44\x7b\xf5\x40\x55\xa6\x8d\xd8\x78\xeb\x37\xfc\x34\x9b\x3f\x4e\x47\xf3\x5f\x6f\x66\xf3\xd1\xe4\xfd\xfc\xee\xc3\xbb\xbb\xf1\xe3\xf8\xae\xe1\x0a\x50\x32\xe9\x31\x85\xdb\xf1\xdb\x8f\xef\x3a\x30\xee\x7f\x9f\x4f\x3e\xdc\x8e\xe7\x93\xe1\xfd\xf8\x34\xee\x67\xab\x8b\xb4\x35\x0d\xf0\x2c\x50\x66\xdb\x4b\xa3\xc3\x32\x65\xb4\x4c\x2b\x1d\x24\xa1\x86\xb0\xed\x1d\x69\x3f\x0d\x7f\x1b\xfd\x52\x25\x9d\x4d\x87\xa3\xff\x32\xf3\xee\x04\x24\xfb\x63\xbb\xf7\x6e\xf5\x8b\xc3\xe4\x9f\x1e\x1d\xb9\x36\x28\x37\x3e\x85\xc1\x55\x71\x38\x0b\xc8\xbd\x15\xb4\x19\x69\x45\xf8\x42\x4d\x6f\x63\x45\x29\x24\xe6\x98\xb5\x34\x0c\x50\x6a\xe9\x0b\xbc\x0f\xea\x6f\x49\xa3\x08\x33\xf5\x6a\xfb\xe1\x04\xf4\xb5\xa1\x3e\x57\xa2\xbf\x10\xea\x44\x22\x5c\x89\x78\x21\x54\x9c\x09\x7b\x09\x02\x89\x57\x10\x0a\x29\xc9\x3a\x41\x14\xd2\xd7\x40\x4a\x66\xfb\x52\xe7\x27\xe1\x52\xe7\x17\x42\x43\x94\xf5\xaa\x9f\x69\xbe\x42\x9b\x38\xcd\x57\x27\x08\xb5\xad\x61\xaa\xb9\x69\x1c\xd9\xf3\xd5\x86\xa5\x55\xa9\x9a\x9c\xd7\xa9\x4f\x89\x8b\x2f\x54\x7c\x01\xa8\x8b\xbe\xf8\x4c\xf5\x17\x60\xda\x04\xc6\xe7\x8a\xff\x2a\xc6\x31\x9d\xc7\x0f\x0b\x66\xc4\xa1\x8b\x9d\x79\x08\x78\x47\xba\x78\xd8\x4a\xfe\x16\x9f\x85\x12\xe1\x42\xed\xe8\x75\xa8\x04\xd7\xea\x59\xe4\x2e\xe9\x7e\x1e\xee\x6e\x75\xc7\x75\xe8\x5b\xdb\xf6\x1f\x01\xe4\xf5\x8b\xe1\xdc\xa3\x72\xd7\x8a\xeb\x2a\x77\x74\x94\x83\xaa\x9d\x0c\x1a\x6d\xe2\xe8\xe8\x38\xd2\xb6\xba\xc0\xb7\x73\xd5\x51\xae\x41\x8c\xf4\x96\xc9\xe6\x9a\xeb\xbe\x23\x54\xee\x25\xb3\x0d\x43\xdd\x96\x2b\x2e\xc6\x93\xf7\xa3\x7a\xee\xaf\x00\x00\x00\xff\xff\xb4\xcc\x66\x9b\xc1\x0b\x00\x00")
 
 func awsNodeYamlBytes() ([]byte, error) {
 	return bindataRead(
@@ -161,6 +182,7 @@ func AssetNames() []string {
 
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
+	"aws-node-1.10.yaml": awsNode110Yaml,
 	"aws-node.yaml": awsNodeYaml,
 	"coredns.yaml": corednsYaml,
 }
@@ -205,6 +227,7 @@ type bintree struct {
 	Children map[string]*bintree
 }
 var _bintree = &bintree{nil, map[string]*bintree{
+	"aws-node-1.10.yaml": &bintree{awsNode110Yaml, map[string]*bintree{}},
 	"aws-node.yaml": &bintree{awsNodeYaml, map[string]*bintree{}},
 	"coredns.yaml": &bintree{corednsYaml, map[string]*bintree{}},
 }}

coolguy741/weave-eksctl

pkg/addons/default/assets.go
@@ -1,5 +1,6 @@
 // Code generated by go-bindata.
 // sources:
+// assets/aws-node-1.10.yaml
 // assets/aws-node.yaml
 // assets/coredns.yaml
 // DO NOT EDIT!
@@ -69,7 +70,27 @@ func (fi bindataFileInfo) Sys() interface{} {
 	return nil
 }
 
-var _awsNodeYaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdd\x6e\xe3\x36\x13\xbd\xd7\x53\x0c\xfc\x5d\x2c\xf0\xa1\x92\xe3\xec\x36\x4d\x75\xe7\x75\xdc\x74\xd1\xc4\x6b\xc4\xdd\x2c\x8a\x45\x60\xd0\xd4\x44\x66\x4d\x91\x2c\x39\x94\xe3\x7d\xfa\x82\x92\x7f\xa4\x58\xf6\xb6\x45\x81\xf2\xc6\x32\x67\xe6\x0c\xe7\xf0\x90\xc3\x38\x8e\x23\x66\xc4\x23\x5a\x27\xb4\x4a\xc1\x2e\x18\x4f\x98\xa7\xa5\xb6\xe2\x2b\x23\xa1\x55\xb2\xba\x76\x89\xd0\xfd\x72\x10\xfd\x0f\x56\x7e\x81\x56\x21\xa1\x83\xb2\x0e\x71\xb0\xc0\x67\x6d\x11\x06\xc9\x75\x72\x01\x6e\xa9\xbd\xcc\xc0\x3b\x3c\x0b\xb5\x40\x62\x83\x68\x25\x54\x96\xc2\x48\x7a\x47\x68\x1f\xb4\xc4\xa8\x40\x62\x19\x23\x96\x46\x00\x8a\x15\x98\x02\x5b\xbb\x58\xe9\x0c\x23\xeb\x25\xba\x34\x8a\x81\x19\x71\x6b\xb5\x37\x2e\x38\xc5\xc0\x6d\x56\xe1\xb2\x82\x7d\xd5\x8a\xad\x5d\xc2\x75\x11\x01\x58\x74\xda\x5b\x8e\x5b\xb7\xde\xff\x7b\xd5\x6f\x40\x75\x86\x71\x74\x11\x84\x1a\x16\x0d\x7b\x13\x1b\xbe\xf4\x7a\x4f\xc7\x30\x46\x67\xae\xc6\xd1\x19\xba\x53\x88\xf0\xa5\x27\x85\xa3\xde\x77\xd0\x5b\x33\xe2\xcb\xf0\x91\x23\xf5\x9e\x5e\xa7\xc0\x17\x42\x55\xd1\xd8\x95\x2c\x63\x58\x68\xe5\x90\xce\x20\x3f\x45\xaf\xb7\xb0\xdc\x11\x3b\x43\x5b\x0a\x8e\x43\xce\xb5\x57\x74\x8e\x5b\x38\x14\x91\x56\x7b\x1c\xbb\x8d\x23\x2c\x8e\xb0\xff\x5b\x79\xbc\x17\x2a\x13\x2a\x3f\xab\x12\x2d\xf1\x01\x9f\x83\x65\x47\xf4\x99\x55\x47\x00\xc7\x1a\x3c\xc2\x74\x7e\xf1\x3b\x72\xaa\xc4\xd7\xc9\xec\xdf\xe3\xb3\x86\xb8\xa9\xf6\x76\x86\xd4\xe2\x97\x19\xe3\xfe\x02\x95\x3f\xb6\xa9\x3c\xa8\x68\xcf\xdd\x3f\xd9\x6c\x00\xc9\x16\x28\x2b\xf1\x01\xac\xae\x5d\xcc\x8c\x69\xf2\x60\x90\x07\x9b\x37\x19\x23\x9c\x91\x65\x84\xf9\xa6\xf6\xa6\x8d\xc1\x14\x1e\xb4\x94\x42\xe5\x9f\x2a\x87\x08\xc0\xa1\x44\x4e\xda\xd6\x3e\x45\x10\xec\x5d\x23\x45\x57\x12\x00\xc2\xc2\x48\x46\xb8\x0d\x6a\x14\x12\x86\x6c\xc5\x77\x23\x84\xc1\x94\xd2\x54\xed\x75\xc3\xd9\xf1\x25\x66\x5e\xa2\x4d\x98\x34\x4b\x96\x1c\x48\x0e\xba\xe3\x56\x90\xe0\x4c\xc6\x46\x67\x29\xbc\x79\x53\x85\xed\x8a\xae\xbe\x5b\xdb\x3e\x79\x4d\x6b\x18\x4b\xed\x68\x82\xb4\xd6\x76\x95\x02\x59\xbf\x9b\x27\x2d\xd1\xb6\x97\x13\x83\x36\x61\x4e\xdb\x14\xc6\x2f\xc2\x55\xa7\x3c\x0c\xae\x15\x31\xa1\xd0\x36\x5c\x45\xc1\x72\x4c\xe1\xea\xe2\xf2\xdd\xc5\x60\xf0\xee\xed\xbb\xef\x2f\x93\x6c\x65\x13\xe4\x36\xf1\x2e\x5e\xa3\xa3\xf8\xb2\x7d\x07\xf6\xeb\x7f\x71\x60\x88\x2b\x91\x96\x83\xe4\x6d\x72\xb9\xe7\xa2\x42\x9c\x7a\x29\xa7\x5a\x0a\xbe\x49\x61\x28\xd7\x6c\xe3\xf6\x76\xa3\x2d\x35\xa8\x8b\x0f\xcb\x9a\x6a\x4b\x29\x5c\x0d\xae\x7e\xb8\xde\x9b\x77\x2a\x2b\x90\xac\xe0\x07\x94\x23\xed\xd5\x03\x55\x99\x36\x62\xe3\xad\xdf\xf0\xf3\x6c\xfe\x38\x1d\xcd\x7f\xb9\x9e\xcd\x47\x93\x0f\xf3\xbb\x8f\xb7\x77\xe3\xc7\xf1\x5d\xc3\x15\xa0\x64\xd2\x63\x0a\x37\xe3\xf7\x9f\x6e\x3b\x30\xee\x7f\x9b\x4f\x3e\xde\x8c\xe7\x93\xe1\xfd\xf8\x38\xee\x27\xab\x8b\xb4\x35\x0d\xf0\x2c\x50\x66\xdb\x4b\xa3\xc3\x32\x65\xb4\x4c\x2b\x1d\x24\xa1\x86\xb0\xed\x1d\x69\x3f\x0f\x7f\x1d\xfd\x5c\x25\x9d\x4d\x87\xa3\x7f\x33\xf3\xee\x04\x24\xfb\x63\xbb\xf7\x6e\xf5\x8b\xc3\xe4\x1f\x1e\x1d\xb9\x36\x28\x37\x3e\x85\xc1\x45\x71\x38\x0b\xc8\xbd\x15\xb4\x19\x69\x45\xf8\x42\x4d\x6f\x63\x45\x29\x24\xe6\x98\xb5\x34\x0c\x50\x6a\xe9\x0b\xbc\x0f\xea\x6f\x49\xa3\x08\x33\xf5\x6a\xfb\xe1\x04\xf4\xb5\xa1\x3e\x57\xa2\xbf\x10\xea\x48\x22\x5c\x89\x78\x21\x54\x9c\x09\x7b\x0e\x02\x89\x57\x10\x0a\x29\xc9\x3a\x41\x14\xd2\xb7\x40\x4a\x66\xfb\x52\xe7\x47\xe1\x52\xe7\x67\x42\x43\x94\xf5\xaa\x9f\x69\xbe\x42\x9b\x38\xcd\x57\x47\x08\xb5\xad\x61\xaa\xb9\x69\x1c\xd9\xd3\xd5\x86\xa5\x55\xa9\x9a\x9c\xd7\xa9\x8f\x89\x8b\xcf\x54\x7c\x06\xa8\x8b\xbe\xf8\x44\xf5\x67\x60\xda\x04\xc6\xa7\x8a\xff\x26\xc6\x6b\x3a\x5f\x3f\x2c\x98\x11\x87\x2e\x76\xe2\x21\xe0\x1d\xe9\xe2\x61\x2b\xf9\x1b\x7c\x16\x4a\x84\x0b\xb5\xa3\xd7\xa1\x12\x5c\xab\x67\x91\xbb\xa4\xfb\x79\xb8\xbb\xd5\x1d\xd7\xa1\x6f\x6d\xdb\x7f\x04\x90\xd7\x2f\x86\x53\x8f\xca\xf2\xf0\xc8\xaa\x3a\xc8\x60\xd7\x4d\xeb\xb2\x8d\xf4\x96\xc9\x66\xfe\xba\x87\x08\x95\x7b\xc9\x6c\xc3\x50\xb7\xd8\xaa\xae\xf1\xe4\xc3\xa8\x9e\x8b\xa2\x3f\x03\x00\x00\xff\xff\xa7\x2d\x3d\xa5\x8f\x0b\x00\x00")
+var _awsNode110Yaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdf\x6f\xdb\x36\x10\x7e\xd7\x5f\x71\x70\x1f\x0a\x0c\x95\x1c\x75\x59\x97\xe9\xcd\x75\xbc\xac\x58\xe2\x1a\xf1\x9a\x62\x28\x02\x83\xa6\x2e\x32\x67\x8a\xe4\xc8\xa3\x1d\xf7\xaf\x1f\x28\xf9\x87\x14\xcb\xee\x36\x0c\x18\x5f\x2c\xf3\xee\xbe\xe3\x7d\xfc\xc8\x63\x1c\xc7\x11\x33\xe2\x01\xad\x13\x5a\x65\x60\xe7\x8c\x27\xcc\xd3\x42\x5b\xf1\x95\x91\xd0\x2a\x59\x5e\xb9\x44\xe8\xfe\x2a\x8d\x5e\xc1\xd2\xcf\xd1\x2a\x24\x74\xb0\xaa\x43\x1c\xcc\xf1\x49\x5b\x84\x34\xb9\x4a\x2e\xc0\x2d\xb4\x97\x39\x78\x87\x67\xa1\xe6\x48\x2c\x8d\x96\x42\xe5\x19\x0c\xa5\x77\x84\xf6\x5e\x4b\x8c\x4a\x24\x96\x33\x62\x59\x04\xa0\x58\x89\x19\xb0\xb5\x8b\x95\xce\x31\xb2\x5e\xa2\xcb\xa2\x18\x98\x11\x37\x56\x7b\xe3\x82\x53\x0c\xdc\xe6\x15\x2e\x2b\xd9\x57\xad\xd8\xda\x25\x5c\x97\x11\x80\x45\xa7\xbd\xe5\xb8\x75\xeb\x7d\xd7\xab\x7e\x03\xaa\x33\x8c\xa3\x8b\x20\xd4\x30\x6f\xd8\x9b\xd8\xf0\xa5\xd7\x7b\x3c\x86\x31\x3a\x77\x35\x8e\xce\xd1\x9d\x42\x84\x2f\x3d\x29\x1c\xf5\xde\x40\x6f\xcd\x88\x2f\xc2\x47\x81\xd4\x7b\x7c\x99\x02\x9f\x09\x55\x45\x63\x57\xb2\x9c\x61\xa9\x95\x43\x3a\x83\xfc\x18\xbd\xdc\xc2\xd5\x8e\xd8\x29\xda\x95\xe0\x38\xe0\x5c\x7b\x45\xe7\xb8\x85\x43\x11\x59\xb5\xc7\xb1\xdb\x38\xc2\xf2\x08\xfb\xff\x95\xc7\x7b\xa1\x72\xa1\x8a\xb3\x2a\xd1\x12\xef\xf1\x29\x58\x76\x44\x9f\x59\x75\x04\x70\xac\xc1\x23\x4c\xe7\xe7\x7f\x20\xa7\x4a\x7c\x9d\xcc\xfe\x33\x3e\x6b\x88\xeb\x6a\x6f\xa7\x48\x2d\x7e\x99\x31\xee\x6f\x50\xf9\x53\x9b\xca\x83\x8a\xf6\xdc\xfd\x9b\xcd\x06\x90\x6c\x8e\xb2\x12\x1f\xc0\xf2\xca\xc5\xcc\x98\x26\x0f\x06\x79\xb0\x79\x93\x33\xc2\x29\x59\x46\x58\x6c\x6a\x6f\xda\x18\xcc\xe0\x5e\x4b\x29\x54\xf1\xa9\x72\x88\x00\x1c\x4a\xe4\xa4\x6d\xed\x53\x06\xc1\xde\x36\x52\x74\x25\x01\x20\x2c\x8d\x64\x84\xdb\xa0\x46\x21\x61\xc8\x56\x7c\x37\x42\x18\x4c\x29\x4d\xd5\x5e\x37\x9c\x1d\x5f\x60\xee\x25\xda\x84\x49\xb3\x60\xc9\x81\xe4\xa0\x3b\x6e\x05\x09\xce\x64\x6c\x74\x9e\xc1\xeb\xd7\x55\xd8\xae\xe8\xea\xbb\xb5\xed\xe3\x97\xb4\x86\xb1\xd0\x8e\xc6\x48\x6b\x6d\x97\x19\x90\xf5\xbb\x79\xd2\x12\x6d\x7b\x39\x31\x68\x13\xe6\xb4\xcd\x60\xf4\x2c\x5c\x75\xca\xc3\xe0\x5a\x11\x13\x0a\x6d\xc3\x55\x94\xac\xc0\x0c\xde\x5d\xbc\xbd\xbc\x48\xd3\xcb\xef\x2f\x7f\x78\x9b\xe4\x4b\x9b\x20\xb7\x89\x77\xf1\x1a\x1d\xc5\x6f\xdb\x77\x60\xbf\xfe\x17\x07\x86\xb8\x12\xd9\x2a\x4d\x2e\x93\x74\xcf\x45\x85\x38\xf1\x52\x4e\xb4\x14\x7c\x93\xc1\x40\xae\xd9\xc6\xed\xed\x46\x5b\x6a\x50\x17\x1f\x96\x35\xd1\x96\x32\x78\x97\xbe\xfb\xf1\x6a\x6f\xde\xa9\xac\x44\xb2\x82\x1f\x50\x8e\xb4\x57\x0f\x54\xab\xac\x11\x1b\x6f\xfd\x06\x9f\xa7\xb3\x87\xc9\x70\xf6\xeb\xd5\x74\x36\x1c\x7f\x98\xdd\x7e\xbc\xb9\x1d\x3d\x8c\x6e\x1b\xae\x00\x2b\x26\x3d\x66\x70\x3d\x7a\xff\xe9\xa6\x03\xe3\xee\xf7\xd9\xf8\xe3\xf5\x68\x36\x1e\xdc\x8d\x8e\xe3\x7e\xb6\xba\xcc\x5a\xd3\x00\x4f\x02\x65\xbe\xbd\x34\x3a\x2c\x13\x46\x8b\xac\xd2\x41\x12\x6a\x08\xdb\xde\x91\xf6\xf3\xe0\xb7\xe1\x2f\x55\xd2\xe9\x64\x30\xfc\x2f\x33\xef\x4e\x40\xb2\x3f\xb6\x7b\xef\x56\xbf\x38\x4c\xfe\xe9\xd1\x91\x6b\x83\x72\xe3\x33\x48\x2f\xca\xc3\x59\x40\xee\xad\xa0\xcd\x50\x2b\xc2\x67\x6a\x7a\x1b\x2b\x56\x42\x62\x81\x79\x4b\xc3\x00\x2b\x2d\x7d\x89\x77\x41\xfd\x2d\x69\x94\x61\xa6\x5e\x6d\x3f\x9c\x80\xbe\x36\xd4\xe7\x4a\xf4\xe7\x42\x1d\x49\x84\x2b\x11\xcf\x85\x8a\x73\x61\xcf\x41\x20\xf1\x0a\x42\x21\x25\x79\x27\x88\x42\xfa\x16\xc8\x8a\xd9\xbe\xd4\xc5\x51\xb8\xd4\xc5\x99\xd0\x10\x65\xbd\xea\xe7\x9a\x2f\xd1\x26\x4e\xf3\xe5\x11\x42\x6d\x6b\x98\x6a\x6e\x1a\x47\xf6\x74\xb5\x61\x69\x55\xaa\x26\xe7\x75\xea\x63\xe2\xe2\x33\x15\x9f\x01\xea\xa2\x2f\x3e\x51\xfd\x19\x98\x36\x81\xf1\xa9\xe2\xbf\x89\xf1\x92\xce\x97\x0f\x0b\x66\xc4\xa1\x8b\x9d\x78\x08\x78\x47\xba\xbc\xdf\x4a\xfe\x1a\x9f\x84\x12\xe1\x42\xed\xe8\x75\xa8\x04\xd7\xea\x49\x14\x2e\xe9\x7e\x1e\xee\x6e\x75\xc7\x75\xe8\x5b\xdb\xf6\x1f\x01\x14\xf5\x8b\xe1\xd4\xa3\xf2\x15\xa4\x49\x7a\x11\x9a\xae\x83\xde\xb6\x2f\xf7\xde\x00\x93\x12\x14\xae\xd1\x56\xed\x78\x67\x70\xbd\xfa\xd9\xb6\x7b\x96\x55\x3d\x27\xdd\xf5\xdf\x9a\x28\x23\xbd\x65\xb2\xb9\xe2\xba\xeb\x08\x55\x78\xc9\x6c\xc3\x50\x37\xe5\x8a\x89\xd1\xf8\xc3\xb0\x9e\xfb\x2b\x00\x00\xff\xff\x9c\xa8\x6b\xec\xbf\x0b\x00\x00")
+
+func awsNode110YamlBytes() ([]byte, error) {
+	return bindataRead(
+		_awsNode110Yaml,
+		"aws-node-1.10.yaml",
+	)
+}
+
+func awsNode110Yaml() (*asset, error) {
+	bytes, err := awsNode110YamlBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "aws-node-1.10.yaml", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _awsNodeYaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xc4\x56\xdf\x8f\x1a\x37\x10\x7e\xdf\xbf\x62\x44\x1f\x22\x55\xdd\xe5\x48\xaf\xe9\x75\xdf\x08\x47\xd3\xa8\x77\x04\x1d\xcd\x45\x55\x74\x42\xc6\x3b\x07\x2e\x5e\xdb\xb5\xc7\x70\xe4\xaf\xaf\xec\xe5\xc7\x2e\x2c\xa4\xad\x2a\xd5\x2f\xb0\x9e\x99\x6f\x3c\x9f\x3f\x7b\x9c\xa6\x69\xc2\x8c\x78\x44\xeb\x84\x56\x39\xd8\x19\xe3\x19\xf3\xb4\xd0\x56\x7c\x61\x24\xb4\xca\x96\x37\x2e\x13\xba\xbb\xea\x25\xdf\xc0\xd2\xcf\xd0\x2a\x24\x74\xb0\xaa\x42\x1c\xcc\xf0\x59\x5b\x84\x5e\x76\x93\x5d\x81\x5b\x68\x2f\x0b\xf0\x0e\x2f\x42\xcd\x90\x58\x2f\x59\x0a\x55\xe4\x30\x90\xde\x11\xda\x07\x2d\x31\x29\x91\x58\xc1\x88\xe5\x09\x80\x62\x25\xe6\xc0\xd6\x2e\x55\xba\xc0\xc4\x7a\x89\x2e\x4f\x52\x60\x46\xbc\xb3\xda\x1b\x17\x9c\x52\xe0\xb6\x88\xb8\xac\x64\x5f\xb4\x62\x6b\x97\x71\x5d\x26\x00\x16\x9d\xf6\x96\xe3\xd6\xad\xf3\x6d\x27\xfe\x06\x54\x67\x18\x47\x97\x40\xa8\x61\x56\xb3\xd7\xb1\xe1\x73\xa7\xf3\x74\x0a\x63\x74\xe1\x2a\x1c\x5d\xa0\x3b\x87\x08\x9f\x3b\x52\x38\xea\x7c\x07\x9d\x35\x23\xbe\x08\x7f\xe6\x48\x9d\xa7\xe3\x14\xf8\x42\xa8\x22\x8d\x6d\xc9\x0a\x86\xa5\x56\x0e\xe9\x02\xf2\x53\x72\xbc\x85\xab\x1d\xb1\x13\xb4\x2b\xc1\xb1\xcf\xb9\xf6\x8a\x2e\x71\x0b\x87\x22\xf2\xb8\xc7\xa9\xdb\x38\xc2\xf2\x04\xfb\xff\x95\xc7\x5b\xa1\x0a\xa1\xe6\x17\x55\xa2\x25\x3e\xe0\x73\xb0\xec\x88\xbe\xb0\xea\x04\xe0\x54\x83\x27\x98\xce\xcf\xfe\x40\x4e\x51\x7c\xad\xcc\xfe\x33\x3e\x2b\x88\xdb\xb8\xb7\x13\xa4\x06\xbf\xcc\x18\xf7\x37\xa8\xfc\xa9\x49\xe5\x41\x45\x7b\xee\xfe\xcd\x66\x03\x48\x36\x43\x19\xc5\x07\xb0\xbc\x71\x29\x33\xa6\xce\x83\x41\x1e\x6c\xde\x14\x8c\x70\x42\x96\x11\xce\x37\x95\x37\x6d\x0c\xe6\xf0\xa0\xa5\x14\x6a\xfe\x31\x3a\x24\x00\x0e\x25\x72\xd2\xb6\xf2\x29\x83\x60\xef\x6a\x29\xda\x92\x00\x10\x96\x46\x32\xc2\x6d\x50\xad\x90\x30\x64\x23\xbe\x1d\x21\x0c\xa6\x94\xa6\xb8\xd7\x35\x67\xc7\x17\x58\x78\x89\x36\x63\xd2\x2c\x58\x76\x20\x39\xe8\x8e\x5b\x41\x82\x33\x99\x1a\x5d\xe4\xf0\xea\x55\x0c\xdb\x15\x1d\x86\xb1\x42\x5b\x41\x9b\x81\x64\xce\x8d\x22\xab\x15\x75\x31\x71\xba\x8b\xdf\x7a\xbb\x86\x48\x46\xc7\x9b\x10\xc6\x42\x3b\x1a\x21\xad\xb5\x5d\xe6\x40\xd6\xef\xe6\x49\x4b\xb4\xcd\xc5\xa7\xa0\x4d\x98\xd3\x36\x87\xe1\x8b\x70\xf1\x4e\x08\x83\x6b\x45\x4c\x28\xb4\x35\x57\x51\xb2\x39\xe6\xf0\xe6\xea\xf5\xf5\x55\xaf\x77\xfd\xfd\xf5\x0f\xaf\xb3\x62\x69\x33\xe4\x36\xf3\x2e\x5d\xa3\xa3\xf4\x75\xf3\xc6\xec\x56\x5f\x69\xe0\x93\x2b\x91\xaf\x7a\xd9\x75\xd6\xdb\x33\x17\x11\xc7\x5e\xca\xb1\x96\x82\x6f\x72\xe8\xcb\x35\xdb\xb8\xbd\xdd\x68\x4b\x35\xa2\xd3\xc3\xb2\xc6\xda\x52\x0e\x6f\x7a\x6f\x7e\xbc\xd9\x9b\x77\x9a\x2c\x91\xac\xe0\x07\x94\x13\xa5\x56\x03\xd5\x2a\xaf\xc5\xa6\x5b\xbf\xfe\xa7\xc9\xf4\x71\x3c\x98\xfe\x7a\x33\x99\x0e\x46\xef\xa7\x77\x1f\xde\xdd\x0d\x1f\x87\x77\x35\x57\x80\x15\x93\x1e\x73\xb8\x1d\xbe\xfd\xf8\xae\x05\xe3\xfe\xf7\xe9\xe8\xc3\xed\x70\x3a\xea\xdf\x0f\x4f\xe3\x7e\xb6\xba\xcc\x1b\xd3\x00\xcf\x02\x65\xb1\xbd\x62\x5a\x2c\x63\x46\x8b\x3c\xaa\x26\x0b\x35\x84\x6d\x6f\x49\xfb\xa9\xff\xdb\xe0\x97\x98\x74\x32\xee\x0f\xfe\xcb\xcc\xbb\xf3\x92\xed\x0f\xf9\xde\xbb\xd1\x5d\x0e\x93\x7f\x7a\x74\xe4\x9a\xa0\xdc\xf8\x1c\x7a\x57\xe5\xe1\xe4\x20\xf7\x51\xfa\x5a\x11\xbe\x50\xdd\xdb\x58\xb1\x12\x12\xe7\x58\x34\x34\x0c\xb0\xd2\xd2\x97\x78\x1f\xd4\xdf\x90\x46\x19\x66\xaa\xd5\x76\xc3\x09\xe8\x6a\x43\x5d\xae\x44\x77\x26\xd4\x89\x44\xb8\x12\xe9\x4c\xa8\xb4\x10\xf6\x12\x04\x12\x8f\x10\x0a\x29\x2b\x5a\x41\x14\xd2\xd7\x40\x56\xcc\x76\xa5\x9e\x9f\x84\x4b\x3d\xbf\x10\x1a\xa2\xac\x57\xdd\x42\xf3\x25\xda\xcc\x69\xbe\x3c\x41\xa8\x6c\x35\x53\xc5\x4d\xed\xc8\x9e\xaf\x36\x2c\x2d\xa6\xaa\x73\x5e\xa5\x3e\x25\x2e\xbd\x50\xf1\x05\xa0\x36\xfa\xd2\x33\xd5\x5f\x80\x69\x12\x98\x9e\x2b\xfe\xab\x18\xc7\x74\x1e\x3f\x43\x98\x11\x87\x9e\x77\xe6\xd9\xe0\x1d\xe9\xf2\x61\x2b\xf9\x5b\x7c\x16\x4a\x84\x0b\xb5\xa5\x33\xa2\x12\x5c\xab\x67\x31\x77\x59\xfb\x63\x72\xd7\x03\x1c\xd7\xa1\xcb\x6d\x1f\x0b\x09\xc0\xbc\x7a\x5f\x9c\x7b\x82\xee\x1a\x77\x55\xe5\x8e\x8e\x55\x2f\x36\x9f\x5e\xad\x4d\x1c\x1d\x1d\x47\xda\xc6\x0b\x7c\x3b\x17\x8f\x72\x05\x62\xa4\xb7\x4c\xd6\xd7\x5c\x75\x29\xa1\xe6\x5e\x32\x5b\x33\x54\x4d\x3c\x72\x31\x1c\xbd\x1f\x54\x73\x7f\x05\x00\x00\xff\xff\xd1\x54\xb9\x74\xef\x0b\x00\x00")
 
 func awsNodeYamlBytes() ([]byte, error) {
 	return bindataRead(
@@ -161,6 +182,7 @@ func AssetNames() []string {
 
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
+	"aws-node-1.10.yaml": awsNode110Yaml,
 	"aws-node.yaml": awsNodeYaml,
 	"coredns.yaml": corednsYaml,
 }
@@ -205,6 +227,7 @@ type bintree struct {
 	Children map[string]*bintree
 }
 var _bintree = &bintree{nil, map[string]*bintree{
+	"aws-node-1.10.yaml": &bintree{awsNode110Yaml, map[string]*bintree{}},
 	"aws-node.yaml": &bintree{awsNodeYaml, map[string]*bintree{}},
 	"coredns.yaml": &bintree{corednsYaml, map[string]*bintree{}},
 }}