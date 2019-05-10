tsigalko2003/eksctl

Add option to allocate all subnets with IPv6 blocks

pkg/apis/eksctl.io/v1alpha5/types.go
@@ -312,6 +312,7 @@ func NewClusterVPC() *ClusterVPC {
 		Network: Network{
 			CIDR: &cidr,
 		},
+		AutoAllocatedIPv6: Disabled(),
 	}
 }
 

pkg/apis/eksctl.io/v1alpha5/vpc.go
@@ -24,6 +24,8 @@ type (
 		ExtraCIDRs []*ipnet.IPNet `json:"extraCIDRs,omitempty"`
 		// for pre-defined shared node SG
 		SharedNodeSecurityGroup string `json:"sharedNodeSecurityGroup,omitempty"`
+		// +optional
+		AutoAllocatedIPv6 *bool `json:"autoAllocatedIPv6,omitempty"`
 	}
 	// ClusterSubnets holds private and public subnets
 	ClusterSubnets struct {

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -224,6 +224,11 @@ func (in *ClusterVPC) DeepCopyInto(out *ClusterVPC) {
 			}
 		}
 	}
+	if in.AutoAllocatedIPv6 != nil {
+		in, out := &in.AutoAllocatedIPv6, &out.AutoAllocatedIPv6
+		*out = new(bool)
+		**out = **in
+	}
 	return
 }
 

coolguy741/weave-eksctl

pkg/apis/eksctl.io/v1alpha5/types.go
@@ -316,6 +316,7 @@ func NewClusterVPC() *ClusterVPC {
 		Network: Network{
 			CIDR: &cidr,
 		},
+		AutoAllocateIPv6: Disabled(),
 	}
 }
 

pkg/apis/eksctl.io/v1alpha5/vpc.go
@@ -24,6 +24,8 @@ type (
 		ExtraCIDRs []*ipnet.IPNet `json:"extraCIDRs,omitempty"`
 		// for pre-defined shared node SG
 		SharedNodeSecurityGroup string `json:"sharedNodeSecurityGroup,omitempty"`
+		// +optional
+		AutoAllocateIPv6 *bool `json:"autoAllocateIPv6,omitempty"`
 	}
 	// ClusterSubnets holds private and public subnets
 	ClusterSubnets struct {

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -224,6 +224,11 @@ func (in *ClusterVPC) DeepCopyInto(out *ClusterVPC) {
 			}
 		}
 	}
+	if in.AutoAllocateIPv6 != nil {
+		in, out := &in.AutoAllocateIPv6, &out.AutoAllocateIPv6
+		*out = new(bool)
+		**out = **in
+	}
 	return
 }
 