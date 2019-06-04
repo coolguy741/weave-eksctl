eksctl-io/eksctl

Add support for spot instances nodegroups

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -260,6 +260,11 @@ func (in *Network) DeepCopy() *Network {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *NodeGroup) DeepCopyInto(out *NodeGroup) {
 	*out = *in
+	if in.SpotInstances != nil {
+		in, out := &in.SpotInstances, &out.SpotInstances
+		*out = new(NodeGroupSpot)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.AvailabilityZones != nil {
 		in, out := &in.AvailabilityZones, &out.AvailabilityZones
 		*out = make([]string, len(*in))
@@ -485,6 +490,27 @@ func (in *NodeGroupSSH) DeepCopy() *NodeGroupSSH {
 		return nil
 	}
 	out := new(NodeGroupSSH)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NodeGroupSpot) DeepCopyInto(out *NodeGroupSpot) {
+	*out = *in
+	if in.MaxPrice != nil {
+		in, out := &in.MaxPrice, &out.MaxPrice
+		*out = new(float64)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NodeGroupSpot.
+func (in *NodeGroupSpot) DeepCopy() *NodeGroupSpot {
+	if in == nil {
+		return nil
+	}
+	out := new(NodeGroupSpot)
 	in.DeepCopyInto(out)
 	return out
 }

coolguy741/weave-eksctl

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -265,6 +265,11 @@ func (in *Network) DeepCopy() *Network {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *NodeGroup) DeepCopyInto(out *NodeGroup) {
 	*out = *in
+	if in.InstancesDistribution != nil {
+		in, out := &in.InstancesDistribution, &out.InstancesDistribution
+		*out = new(NodeGroupInstancesDistribution)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.AvailabilityZones != nil {
 		in, out := &in.AvailabilityZones, &out.AvailabilityZones
 		*out = make([]string, len(*in))
@@ -295,6 +300,21 @@ func (in *NodeGroup) DeepCopyInto(out *NodeGroup) {
 	if in.MaxSize != nil {
 		in, out := &in.MaxSize, &out.MaxSize
 		*out = new(int)
+		**out = **in
+	}
+	if in.VolumeSize != nil {
+		in, out := &in.VolumeSize, &out.VolumeSize
+		*out = new(int)
+		**out = **in
+	}
+	if in.VolumeType != nil {
+		in, out := &in.VolumeType, &out.VolumeType
+		*out = new(string)
+		**out = **in
+	}
+	if in.VolumeName != nil {
+		in, out := &in.VolumeName, &out.VolumeName
+		*out = new(string)
 		**out = **in
 	}
 	if in.Labels != nil {
@@ -433,6 +453,47 @@ func (in *NodeGroupIAMAddonPolicies) DeepCopy() *NodeGroupIAMAddonPolicies {
 		return nil
 	}
 	out := new(NodeGroupIAMAddonPolicies)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NodeGroupInstancesDistribution) DeepCopyInto(out *NodeGroupInstancesDistribution) {
+	*out = *in
+	if in.InstanceTypes != nil {
+		in, out := &in.InstanceTypes, &out.InstanceTypes
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.MaxPrice != nil {
+		in, out := &in.MaxPrice, &out.MaxPrice
+		*out = new(float64)
+		**out = **in
+	}
+	if in.OnDemandBaseCapacity != nil {
+		in, out := &in.OnDemandBaseCapacity, &out.OnDemandBaseCapacity
+		*out = new(int)
+		**out = **in
+	}
+	if in.OnDemandPercentageAboveBaseCapacity != nil {
+		in, out := &in.OnDemandPercentageAboveBaseCapacity, &out.OnDemandPercentageAboveBaseCapacity
+		*out = new(int)
+		**out = **in
+	}
+	if in.SpotInstancePools != nil {
+		in, out := &in.SpotInstancePools, &out.SpotInstancePools
+		*out = new(int)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NodeGroupInstancesDistribution.
+func (in *NodeGroupInstancesDistribution) DeepCopy() *NodeGroupInstancesDistribution {
+	if in == nil {
+		return nil
+	}
+	out := new(NodeGroupInstancesDistribution)
 	in.DeepCopyInto(out)
 	return out
 }