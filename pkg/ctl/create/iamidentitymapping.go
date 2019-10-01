eksctl-io/eksctl

Support users in get/create/delete iamidentitymapping

pkg/ctl/create/iamidentitymapping.go
@@ -15,10 +15,10 @@ func createIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	cfg := api.NewClusterConfig()
 	rc.ClusterConfig = cfg
 
-	id := &authconfigmap.MapRole{}
+	id := &authconfigmap.MapIdentity{}
 
 	rc.SetDescription("iamidentitymapping", "Create an IAM identity mapping",
-		dedent.Dedent(`Creates a mapping from IAM role to Kubernetes user and groups.
+		dedent.Dedent(`Creates a mapping from IAM role or user to Kubernetes user and groups.
 
 			Note aws-iam-authenticator only considers the last entry for any given
 			role. If you create a duplicate entry it will shadow all the previous
@@ -31,7 +31,7 @@ func createIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	})
 
 	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&id.RoleARN, "role", "", "ARN of the IAM role to create")
+		fs.StringVar(&id.ARN, "arn", "", "ARN of the IAM role or user to create")
 		fs.StringVar(&id.Username, "username", "", "User name within Kubernetes to map to IAM role")
 		fs.StringArrayVar(&id.Groups, "group", []string{}, "Group within Kubernetes to which IAM role is mapped")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
@@ -42,7 +42,7 @@ func createIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	cmdutils.AddCommonFlagsForAWS(rc.FlagSetGroup, rc.ProviderConfig, false)
 }
 
-func doCreateIAMIdentityMapping(rc *cmdutils.ResourceCmd, id *authconfigmap.MapRole) error {
+func doCreateIAMIdentityMapping(rc *cmdutils.ResourceCmd, id *authconfigmap.MapIdentity) error {
 	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
@@ -54,8 +54,8 @@ func doCreateIAMIdentityMapping(rc *cmdutils.ResourceCmd, id *authconfigmap.MapR
 	if err := ctl.CheckAuth(); err != nil {
 		return err
 	}
-	if id.RoleARN == "" {
-		return cmdutils.ErrMustBeSet("--role")
+	if id.ARN == "" {
+		return cmdutils.ErrMustBeSet("--arn")
 	}
 	if cfg.Metadata.Name == "" {
 		return cmdutils.ErrMustBeSet("--name")
@@ -77,16 +77,16 @@ func doCreateIAMIdentityMapping(rc *cmdutils.ResourceCmd, id *authconfigmap.MapR
 	}
 
 	// Check whether role already exists.
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	filtered := roles.Get(id.RoleARN)
+	filtered := identities.Get(id.ARN)
 	if len(filtered) > 0 {
-		logger.Warning("found %d mappings with same role %q (which will be shadowed by your new mapping)", len(filtered), id.RoleARN)
+		logger.Warning("found %d mappings with same arn %q (which will be shadowed by your new mapping)", len(filtered), id.ARN)
 	}
 
-	if err := acm.AddRole(id.RoleARN, id.Username, id.Groups); err != nil {
+	if err := acm.AddIdentity(id.ARN, id.Username, id.Groups); err != nil {
 		return err
 	}
 	return acm.Save()

pkg/ctl/delete/iamidentitymapping.go
@@ -15,18 +15,18 @@ func deleteIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	rc.ClusterConfig = cfg
 
 	var (
-		role string
-		all  bool
+		arn string
+		all bool
 	)
 
 	rc.SetDescription("iamidentitymapping", "Delete a IAM identity mapping", "")
 
 	rc.SetRunFunc(func() error {
-		return doDeleteIAMIdentityMapping(rc, role, all)
+		return doDeleteIAMIdentityMapping(rc, arn, all)
 	})
 
 	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&role, "role", "", "ARN of the IAM role to delete")
+		fs.StringVar(&arn, "arn", "", "ARN of the IAM role or user to delete")
 		fs.BoolVar(&all, "all", false, "Delete all matching mappings instead of just one")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
 		cmdutils.AddRegionFlag(fs, rc.ProviderConfig)
@@ -36,7 +36,7 @@ func deleteIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	cmdutils.AddCommonFlagsForAWS(rc.FlagSetGroup, rc.ProviderConfig, false)
 }
 
-func doDeleteIAMIdentityMapping(rc *cmdutils.ResourceCmd, role string, all bool) error {
+func doDeleteIAMIdentityMapping(rc *cmdutils.ResourceCmd, arn string, all bool) error {
 	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
@@ -49,7 +49,7 @@ func doDeleteIAMIdentityMapping(rc *cmdutils.ResourceCmd, role string, all bool)
 		return err
 	}
 
-	if role == "" {
+	if arn == "" {
 		return cmdutils.ErrMustBeSet("--role")
 	}
 	if cfg.Metadata.Name == "" {
@@ -68,7 +68,7 @@ func doDeleteIAMIdentityMapping(rc *cmdutils.ResourceCmd, role string, all bool)
 		return err
 	}
 
-	if err := acm.RemoveRole(role, all); err != nil {
+	if err := acm.RemoveIdentity(arn, all); err != nil {
 		return err
 	}
 	if err := acm.Save(); err != nil {
@@ -76,13 +76,13 @@ func doDeleteIAMIdentityMapping(rc *cmdutils.ResourceCmd, role string, all bool)
 	}
 
 	// Check whether we have more roles that match
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	filtered := roles.Get(role)
+	filtered := identities.Get(arn)
 	if len(filtered) > 0 {
-		logger.Warning("there are %d mappings left with same role %q (use --all to delete them at once)", len(filtered), role)
+		logger.Warning("there are %d mappings left with same arn %q (use --all to delete them at once)", len(filtered), arn)
 	}
 	return nil
 }

pkg/ctl/get/iamidentitymapping.go
@@ -18,18 +18,18 @@ func getIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	cfg := api.NewClusterConfig()
 	rc.ClusterConfig = cfg
 
-	var role string
+	var arn string
 
 	params := &getCmdParams{}
 
 	rc.SetDescription("iamidentitymapping", "Get IAM identity mapping(s)", "")
 
 	rc.SetRunFunc(func() error {
-		return doGetIAMIdentityMapping(rc, params, role)
+		return doGetIAMIdentityMapping(rc, params, arn)
 	})
 
 	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&role, "role", "", "ARN of the IAM role")
+		fs.StringVar(&arn, "arn", "", "ARN of the IAM role or user")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
 		cmdutils.AddRegionFlag(fs, rc.ProviderConfig)
 		cmdutils.AddCommonFlagsForGetCmd(fs, &params.chunkSize, &params.output)
@@ -39,7 +39,7 @@ func getIAMIdentityMappingCmd(rc *cmdutils.ResourceCmd) {
 	cmdutils.AddCommonFlagsForAWS(rc.FlagSetGroup, rc.ProviderConfig, false)
 }
 
-func doGetIAMIdentityMapping(rc *cmdutils.ResourceCmd, params *getCmdParams, role string) error {
+func doGetIAMIdentityMapping(rc *cmdutils.ResourceCmd, params *getCmdParams, arn string) error {
 	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
@@ -67,15 +67,15 @@ func doGetIAMIdentityMapping(rc *cmdutils.ResourceCmd, params *getCmdParams, rol
 	if err != nil {
 		return err
 	}
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	if role != "" {
-		roles = roles.Get(role)
+	if arn != "" {
+		identities = identities.Get(arn)
 		// If a filter was given, we error if none was found
-		if len(roles) == 0 {
-			return fmt.Errorf("no iamidentitymapping with role %q found", role)
+		if len(identities) == 0 {
+			return fmt.Errorf("no iamidentitymapping with arn %q found", arn)
 		}
 	}
 
@@ -87,7 +87,7 @@ func doGetIAMIdentityMapping(rc *cmdutils.ResourceCmd, params *getCmdParams, rol
 		addIAMIdentityMappingTableColumns(printer.(*printers.TablePrinter))
 	}
 
-	if err := printer.PrintObjWithKind("iamidentitymappings", roles, os.Stdout); err != nil {
+	if err := printer.PrintObjWithKind("iamidentitymappings", identities, os.Stdout); err != nil {
 		return err
 	}
 
@@ -95,13 +95,13 @@ func doGetIAMIdentityMapping(rc *cmdutils.ResourceCmd, params *getCmdParams, rol
 }
 
 func addIAMIdentityMappingTableColumns(printer *printers.TablePrinter) {
-	printer.AddColumn("ROLE", func(r authconfigmap.MapRole) string {
-		return r.RoleARN
+	printer.AddColumn("ARN", func(r authconfigmap.MapIdentity) string {
+		return r.ARN
 	})
-	printer.AddColumn("USERNAME", func(r authconfigmap.MapRole) string {
+	printer.AddColumn("USERNAME", func(r authconfigmap.MapIdentity) string {
 		return r.Username
 	})
-	printer.AddColumn("GROUPS", func(r authconfigmap.MapRole) string {
+	printer.AddColumn("GROUPS", func(r authconfigmap.MapIdentity) string {
 		return strings.Join(r.Groups, ",")
 	})
 }

coolguy741/weave-eksctl

pkg/ctl/create/iamidentitymapping.go
@@ -14,10 +14,10 @@ func createIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cfg := api.NewClusterConfig()
 	cmd.ClusterConfig = cfg
 
-	id := &authconfigmap.MapRole{}
+	id := &authconfigmap.MapIdentity{}
 
-	cmd.SetDescription("iamidentitymapping", "Create an IAM identity mapping",
-		dedent.Dedent(`Creates a mapping from IAM role to Kubernetes user and groups.
+	rc.SetDescription("iamidentitymapping", "Create an IAM identity mapping",
+		dedent.Dedent(`Creates a mapping from IAM role or user to Kubernetes user and groups.
 
 			Note aws-iam-authenticator only considers the last entry for any given
 			role. If you create a duplicate entry it will shadow all the previous
@@ -29,8 +29,8 @@ func createIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 		return doCreateIAMIdentityMapping(cmd, id)
 	})
 
-	cmd.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&id.RoleARN, "role", "", "ARN of the IAM role to create")
+	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
+		fs.StringVar(&id.ARN, "arn", "", "ARN of the IAM role or user to create")
 		fs.StringVar(&id.Username, "username", "", "User name within Kubernetes to map to IAM role")
 		fs.StringArrayVar(&id.Groups, "group", []string{}, "Group within Kubernetes to which IAM role is mapped")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
@@ -42,8 +42,8 @@ func createIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cmdutils.AddCommonFlagsForAWS(cmd.FlagSetGroup, cmd.ProviderConfig, false)
 }
 
-func doCreateIAMIdentityMapping(cmd *cmdutils.Cmd, id *authconfigmap.MapRole) error {
-	if err := cmdutils.NewMetadataLoader(cmd).Load(); err != nil {
+func doCreateIAMIdentityMapping(rc *cmdutils.Cmd, id *authconfigmap.MapIdentity) error {
+	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
 
@@ -58,8 +58,8 @@ func doCreateIAMIdentityMapping(cmd *cmdutils.Cmd, id *authconfigmap.MapRole) er
 	if err := ctl.CheckAuth(); err != nil {
 		return err
 	}
-	if id.RoleARN == "" {
-		return cmdutils.ErrMustBeSet("--role")
+	if id.ARN == "" {
+		return cmdutils.ErrMustBeSet("--arn")
 	}
 	if cfg.Metadata.Name == "" {
 		return cmdutils.ErrMustBeSet("--name")
@@ -81,16 +81,16 @@ func doCreateIAMIdentityMapping(cmd *cmdutils.Cmd, id *authconfigmap.MapRole) er
 	}
 
 	// Check whether role already exists.
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	filtered := roles.Get(id.RoleARN)
+	filtered := identities.Get(id.ARN)
 	if len(filtered) > 0 {
-		logger.Warning("found %d mappings with same role %q (which will be shadowed by your new mapping)", len(filtered), id.RoleARN)
+		logger.Warning("found %d mappings with same arn %q (which will be shadowed by your new mapping)", len(filtered), id.ARN)
 	}
 
-	if err := acm.AddRole(id.RoleARN, id.Username, id.Groups); err != nil {
+	if err := acm.AddIdentity(id.ARN, id.Username, id.Groups); err != nil {
 		return err
 	}
 	return acm.Save()

pkg/ctl/delete/iamidentitymapping.go
@@ -14,18 +14,18 @@ func deleteIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cmd.ClusterConfig = cfg
 
 	var (
-		role string
-		all  bool
+		arn string
+		all bool
 	)
 
 	cmd.SetDescription("iamidentitymapping", "Delete a IAM identity mapping", "")
 
-	cmd.SetRunFunc(func() error {
-		return doDeleteIAMIdentityMapping(cmd, role, all)
+	rc.SetRunFunc(func() error {
+		return doDeleteIAMIdentityMapping(rc, arn, all)
 	})
 
-	cmd.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&role, "role", "", "ARN of the IAM role to delete")
+	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
+		fs.StringVar(&arn, "arn", "", "ARN of the IAM role or user to delete")
 		fs.BoolVar(&all, "all", false, "Delete all matching mappings instead of just one")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
 		cmdutils.AddRegionFlag(fs, cmd.ProviderConfig)
@@ -36,8 +36,8 @@ func deleteIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cmdutils.AddCommonFlagsForAWS(cmd.FlagSetGroup, cmd.ProviderConfig, false)
 }
 
-func doDeleteIAMIdentityMapping(cmd *cmdutils.Cmd, role string, all bool) error {
-	if err := cmdutils.NewMetadataLoader(cmd).Load(); err != nil {
+func doDeleteIAMIdentityMapping(rc *cmdutils.Cmd, arn string, all bool) error {
+	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
 
@@ -53,7 +53,7 @@ func doDeleteIAMIdentityMapping(cmd *cmdutils.Cmd, role string, all bool) error 
 		return err
 	}
 
-	if role == "" {
+	if arn == "" {
 		return cmdutils.ErrMustBeSet("--role")
 	}
 	if cfg.Metadata.Name == "" {
@@ -72,7 +72,7 @@ func doDeleteIAMIdentityMapping(cmd *cmdutils.Cmd, role string, all bool) error 
 		return err
 	}
 
-	if err := acm.RemoveRole(role, all); err != nil {
+	if err := acm.RemoveIdentity(arn, all); err != nil {
 		return err
 	}
 	if err := acm.Save(); err != nil {
@@ -80,13 +80,13 @@ func doDeleteIAMIdentityMapping(cmd *cmdutils.Cmd, role string, all bool) error 
 	}
 
 	// Check whether we have more roles that match
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	filtered := roles.Get(role)
+	filtered := identities.Get(arn)
 	if len(filtered) > 0 {
-		logger.Warning("there are %d mappings left with same role %q (use --all to delete them at once)", len(filtered), role)
+		logger.Warning("there are %d mappings left with same arn %q (use --all to delete them at once)", len(filtered), arn)
 	}
 	return nil
 }

pkg/ctl/get/iamidentitymapping.go
@@ -17,18 +17,18 @@ func getIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cfg := api.NewClusterConfig()
 	cmd.ClusterConfig = cfg
 
-	var role string
+	var arn string
 
 	params := &getCmdParams{}
 
 	cmd.SetDescription("iamidentitymapping", "Get IAM identity mapping(s)", "")
 
-	cmd.SetRunFunc(func() error {
-		return doGetIAMIdentityMapping(cmd, params, role)
+	rc.SetRunFunc(func() error {
+		return doGetIAMIdentityMapping(rc, params, arn)
 	})
 
-	cmd.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
-		fs.StringVar(&role, "role", "", "ARN of the IAM role")
+	rc.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
+		fs.StringVar(&arn, "arn", "", "ARN of the IAM role or user")
 		cmdutils.AddNameFlag(fs, cfg.Metadata)
 		cmdutils.AddRegionFlag(fs, cmd.ProviderConfig)
 		cmdutils.AddCommonFlagsForGetCmd(fs, &params.chunkSize, &params.output)
@@ -39,8 +39,8 @@ func getIAMIdentityMappingCmd(cmd *cmdutils.Cmd) {
 	cmdutils.AddCommonFlagsForAWS(cmd.FlagSetGroup, cmd.ProviderConfig, false)
 }
 
-func doGetIAMIdentityMapping(cmd *cmdutils.Cmd, params *getCmdParams, role string) error {
-	if err := cmdutils.NewMetadataLoader(cmd).Load(); err != nil {
+func doGetIAMIdentityMapping(rc *cmdutils.Cmd, params *getCmdParams, arn string) error {
+	if err := cmdutils.NewMetadataLoader(rc).Load(); err != nil {
 		return err
 	}
 
@@ -70,15 +70,15 @@ func doGetIAMIdentityMapping(cmd *cmdutils.Cmd, params *getCmdParams, role strin
 	if err != nil {
 		return err
 	}
-	roles, err := acm.Roles()
+	identities, err := acm.Identities()
 	if err != nil {
 		return err
 	}
-	if role != "" {
-		roles = roles.Get(role)
+	if arn != "" {
+		identities = identities.Get(arn)
 		// If a filter was given, we error if none was found
-		if len(roles) == 0 {
-			return fmt.Errorf("no iamidentitymapping with role %q found", role)
+		if len(identities) == 0 {
+			return fmt.Errorf("no iamidentitymapping with arn %q found", arn)
 		}
 	}
 
@@ -90,7 +90,7 @@ func doGetIAMIdentityMapping(cmd *cmdutils.Cmd, params *getCmdParams, role strin
 		addIAMIdentityMappingTableColumns(printer.(*printers.TablePrinter))
 	}
 
-	if err := printer.PrintObjWithKind("iamidentitymappings", roles, os.Stdout); err != nil {
+	if err := printer.PrintObjWithKind("iamidentitymappings", identities, os.Stdout); err != nil {
 		return err
 	}
 
@@ -98,13 +98,13 @@ func doGetIAMIdentityMapping(cmd *cmdutils.Cmd, params *getCmdParams, role strin
 }
 
 func addIAMIdentityMappingTableColumns(printer *printers.TablePrinter) {
-	printer.AddColumn("ROLE", func(r authconfigmap.MapRole) string {
-		return r.RoleARN
+	printer.AddColumn("ARN", func(r authconfigmap.MapIdentity) string {
+		return r.ARN
 	})
-	printer.AddColumn("USERNAME", func(r authconfigmap.MapRole) string {
+	printer.AddColumn("USERNAME", func(r authconfigmap.MapIdentity) string {
 		return r.Username
 	})
-	printer.AddColumn("GROUPS", func(r authconfigmap.MapRole) string {
+	printer.AddColumn("GROUPS", func(r authconfigmap.MapIdentity) string {
 		return strings.Join(r.Groups, ",")
 	})
 }