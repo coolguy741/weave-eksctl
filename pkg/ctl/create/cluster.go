ndegory/eksctl

add commands/infra to change the endpoint access

pkg/ctl/create/cluster.go
@@ -19,13 +19,12 @@ import (
 )
 
 type createClusterCmdParams struct {
-	writeKubeconfig      bool
-	kubeconfigPath       string
-	autoKubeconfigPath   bool
-	authenticatorRoleARN string
-	setContext           bool
-	availabilityZones    []string
-
+	writeKubeconfig       bool
+	kubeconfigPath        string
+	autoKubeconfigPath    bool
+	authenticatorRoleARN  string
+	setContext            bool
+	availabilityZones     []string
 	kopsClusterNameForVPC string
 	subnets               map[api.SubnetTopology]*[]string
 	withoutNodeGroup      bool
@@ -115,6 +114,10 @@ func doCreateCluster(cmd *cmdutils.Cmd, ng *api.NodeGroup, params *createCluster
 		}
 	}
 
+	if err := cfg.ValidateClusterEndpointConfig(); err != nil {
+		return err
+	}
+	
 	if err := ctl.CheckAuth(); err != nil {
 		return err
 	}

coolguy741/weave-eksctl

pkg/ctl/create/cluster.go
@@ -27,10 +27,9 @@ type createClusterCmdParams struct {
 	setContext                  bool
 	availabilityZones           []string
 	installWindowsVPCController bool
-
-	kopsClusterNameForVPC string
-	subnets               map[api.SubnetTopology]*[]string
-	withoutNodeGroup      bool
+	kopsClusterNameForVPC       string
+	subnets                     map[api.SubnetTopology]*[]string
+	withoutNodeGroup            bool
 }
 
 func createClusterCmd(cmd *cmdutils.Cmd) {
@@ -116,6 +115,10 @@ func doCreateCluster(cmd *cmdutils.Cmd, ng *api.NodeGroup, params *createCluster
 			}
 			return fmt.Errorf("invalid version, supported values: %s", strings.Join(api.SupportedVersions(), ", "))
 		}
+	}
+
+	if err := cfg.ValidateClusterEndpointConfig(); err != nil {
+		return err
 	}
 
 	if err := ctl.CheckAuth(); err != nil {