ndegory/eksctl

Use image as cache store
- there is no need to build the image in CI as a primary job
  - pulling the image is as quick as restoring cache
  - we can control what is being cached
  - we don't have to rely on how CircleCI cache works, so our
    jobs are more portable
- cleanup
  - remove outdated Skaffold config
  - cleanup Dockerfile, cache all of the modules, use GOPROXY
- upgrade CircleCI jobs' resource class
- start trial of GitHub Actions in parallel to CircleCI
  - start with two workflows
    - one is for running tests & building the binaries in pre-existing image
    - one for updating images
- split Docker-related targets from the main Makefile, avoiding
  having to have Go installed in CI job that builds images
- extract common variables into separate `Makefile.common`
- update modules that are used in the build
- fix file license header filename

install-build-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 # Make sure to run the following commands after changes to this file are made:
-# `make update-build-image-manifest && make push-build-image`
+# `make -f Makefile.docker update-build-image-manifest && make -f Makefile.docker push-build-image`
 
 if [ -z "${GOBIN+x}" ]; then
  GOBIN="$(go env GOPATH)/bin"
@@ -15,10 +15,17 @@ fi
 
 env CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo
 
-go install github.com/jteeuwen/go-bindata/go-bindata
-go install github.com/vektra/mockery/cmd/mockery
-go install github.com/weaveworks/github-release
-go install golang.org/x/tools/cmd/stringer
+go install \
+  github.com/goreleaser/goreleaser \
+  github.com/jteeuwen/go-bindata/go-bindata \
+  github.com/vektra/mockery/cmd/mockery \
+  github.com/weaveworks/github-release \
+  golang.org/x/tools/cmd/stringer \
+  k8s.io/code-generator/cmd/client-gen \
+  k8s.io/code-generator/cmd/deepcopy-gen \
+  k8s.io/code-generator/cmd/defaulter-gen \
+  k8s.io/code-generator/cmd/informer-gen \
+  k8s.io/code-generator/cmd/lister-gen
 
 # TODO: metalinter is archived, we should switch to github.com/golangci/golangci-lint
 # Install metalinter

coolguy741/weave-eksctl

install-build-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 # Make sure to run the following commands after changes to this file are made:
-# `make update-build-image-manifest && make push-build-image`
+# `make -f Makefile.docker update-build-image-manifest && make -f Makefile.docker push-build-image`
 
 if [ -z "${GOBIN+x}" ]; then
  GOBIN="$(go env GOPATH)/bin"
@@ -15,10 +15,17 @@ fi
 
 env CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo
 
-go install github.com/kevinburke/go-bindata/go-bindata
-go install github.com/vektra/mockery/cmd/mockery
-go install github.com/weaveworks/github-release
-go install golang.org/x/tools/cmd/stringer
+go install \
+  github.com/goreleaser/goreleaser \
+  github.com/kevinburke/go-bindata/go-bindata \
+  github.com/vektra/mockery/cmd/mockery \
+  github.com/weaveworks/github-release \
+  golang.org/x/tools/cmd/stringer \
+  k8s.io/code-generator/cmd/client-gen \
+  k8s.io/code-generator/cmd/deepcopy-gen \
+  k8s.io/code-generator/cmd/defaulter-gen \
+  k8s.io/code-generator/cmd/informer-gen \
+  k8s.io/code-generator/cmd/lister-gen
 
 # TODO: metalinter is archived, we should switch to github.com/golangci/golangci-lint
 # Install metalinter