s-tokutake/eksctl

Add generated files post GitOps fields addition

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -112,6 +112,11 @@ func (in *ClusterConfig) DeepCopyInto(out *ClusterConfig) {
 		*out = new(ClusterStatus)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.GitOps != nil {
+		in, out := &in.GitOps, &out.GitOps
+		*out = new(GitOps)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
@@ -416,6 +421,32 @@ func (in *ClusterVPC) DeepCopy() *ClusterVPC {
 		return nil
 	}
 	out := new(ClusterVPC)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *GitOps) DeepCopyInto(out *GitOps) {
+	*out = *in
+	if in.Repo != nil {
+		in, out := &in.Repo, &out.Repo
+		*out = new(Repo)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Profiles != nil {
+		in, out := &in.Profiles, &out.Profiles
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new GitOps.
+func (in *GitOps) DeepCopy() *GitOps {
+	if in == nil {
+		return nil
+	}
+	out := new(GitOps)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -794,6 +825,27 @@ func (in *ProviderConfig) DeepCopy() *ProviderConfig {
 		return nil
 	}
 	out := new(ProviderConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Repo) DeepCopyInto(out *Repo) {
+	*out = *in
+	if in.Paths != nil {
+		in, out := &in.Paths, &out.Paths
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Repo.
+func (in *Repo) DeepCopy() *Repo {
+	if in == nil {
+		return nil
+	}
+	out := new(Repo)
 	in.DeepCopyInto(out)
 	return out
 }

coolguy741/weave-eksctl

pkg/apis/eksctl.io/v1alpha5/zz_generated.deepcopy.go
@@ -112,6 +112,11 @@ func (in *ClusterConfig) DeepCopyInto(out *ClusterConfig) {
 		*out = new(ClusterStatus)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Git != nil {
+		in, out := &in.Git, &out.Git
+		*out = new(Git)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
@@ -416,6 +421,43 @@ func (in *ClusterVPC) DeepCopy() *ClusterVPC {
 		return nil
 	}
 	out := new(ClusterVPC)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Git) DeepCopyInto(out *Git) {
+	*out = *in
+	if in.Repo != nil {
+		in, out := &in.Repo, &out.Repo
+		*out = new(Repo)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Operator != nil {
+		in, out := &in.Operator, &out.Operator
+		*out = new(Operator)
+		**out = **in
+	}
+	if in.Profiles != nil {
+		in, out := &in.Profiles, &out.Profiles
+		*out = make([]*Profile, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(Profile)
+				**out = **in
+			}
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Git.
+func (in *Git) DeepCopy() *Git {
+	if in == nil {
+		return nil
+	}
+	out := new(Git)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -783,6 +825,38 @@ func (in *NodeGroupSSH) DeepCopy() *NodeGroupSSH {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Operator) DeepCopyInto(out *Operator) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Operator.
+func (in *Operator) DeepCopy() *Operator {
+	if in == nil {
+		return nil
+	}
+	out := new(Operator)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Profile) DeepCopyInto(out *Profile) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Profile.
+func (in *Profile) DeepCopy() *Profile {
+	if in == nil {
+		return nil
+	}
+	out := new(Profile)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *ProviderConfig) DeepCopyInto(out *ProviderConfig) {
 	*out = *in
 	return
@@ -794,6 +868,27 @@ func (in *ProviderConfig) DeepCopy() *ProviderConfig {
 		return nil
 	}
 	out := new(ProviderConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Repo) DeepCopyInto(out *Repo) {
+	*out = *in
+	if in.Paths != nil {
+		in, out := &in.Paths, &out.Paths
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Repo.
+func (in *Repo) DeepCopy() *Repo {
+	if in == nil {
+		return nil
+	}
+	out := new(Repo)
 	in.DeepCopyInto(out)
 	return out
 }