veekaly/eksctl

Migrate usage of EC2 to aws-sdk-go-v2

pkg/cfn/builder/vpc_existing.go
@@ -1,30 +1,35 @@
 package builder
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+	"github.com/weaveworks/eksctl/pkg/awsapi"
+
+	ec22 "github.com/aws/aws-sdk-go-v2/service/ec2"
+
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	awsec2 "github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/pkg/errors"
+	gfnt "github.com/weaveworks/goformation/v4/cloudformation/types"
+
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/cfn/outputs"
 	"github.com/weaveworks/eksctl/pkg/vpc"
-	gfnt "github.com/weaveworks/goformation/v4/cloudformation/types"
 )
 
 type ExistingVPCResourceSet struct {
 	rs            *resourceSet
 	clusterConfig *api.ClusterConfig
-	ec2API        ec2iface.EC2API
+	ec2API        awsapi.EC2
 	vpcID         *gfnt.Value
 	subnetDetails *SubnetDetails
 }
 
 // NewExistingVPCResourceSet creates and returns a new VPCResourceSet
-func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig, ec2API ec2iface.EC2API) *ExistingVPCResourceSet {
+func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig, ec2API awsapi.EC2) *ExistingVPCResourceSet {
 	return &ExistingVPCResourceSet{
 		rs:            rs,
 		clusterConfig: clusterConfig,
@@ -34,9 +39,9 @@ func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig
 	}
 }
 
-func (v *ExistingVPCResourceSet) CreateTemplate() (*gfnt.Value, *SubnetDetails, error) {
-	out, err := v.ec2API.DescribeVpcs(&awsec2.DescribeVpcsInput{
-		VpcIds: aws.StringSlice([]string{v.clusterConfig.VPC.ID}),
+func (v *ExistingVPCResourceSet) CreateTemplate(ctx context.Context) (*gfnt.Value, *SubnetDetails, error) {
+	out, err := v.ec2API.DescribeVpcs(ctx, &ec22.DescribeVpcsInput{
+		VpcIds: []string{v.clusterConfig.VPC.ID},
 	})
 
 	if err != nil {
@@ -52,16 +57,16 @@ func (v *ExistingVPCResourceSet) CreateTemplate() (*gfnt.Value, *SubnetDetails, 
 			return nil, nil, err
 		}
 	}
-	if err := v.importExistingResources(); err != nil {
+	if err := v.importExistingResources(ctx); err != nil {
 		return nil, nil, errors.Wrap(err, "error importing VPC resources")
 	}
 
-	v.addOutputs()
+	v.addOutputs(ctx)
 	return v.vpcID, v.subnetDetails, nil
 }
 
 // addOutputs adds VPC resource outputs
-func (v *ExistingVPCResourceSet) addOutputs() {
+func (v *ExistingVPCResourceSet) addOutputs(ctx context.Context) {
 	v.rs.defineOutput(outputs.ClusterVPC, v.vpcID, true, func(val string) error {
 		v.clusterConfig.VPC.ID = val
 		return nil
@@ -73,7 +78,7 @@ func (v *ExistingVPCResourceSet) addOutputs() {
 
 	addSubnetOutput := func(subnetRefs []*gfnt.Value, topology api.SubnetTopology, outputName string) {
 		v.rs.defineJoinedOutput(outputName, subnetRefs, true, func(value string) error {
-			return vpc.ImportSubnetsFromIDList(v.ec2API, v.clusterConfig, topology, strings.Split(value, ","))
+			return vpc.ImportSubnetsFromIDList(ctx, v.ec2API, v.clusterConfig, topology, strings.Split(value, ","))
 		})
 	}
 
@@ -90,21 +95,21 @@ func (v *ExistingVPCResourceSet) addOutputs() {
 	}
 }
 
-func (v *ExistingVPCResourceSet) checkIPv6CidrBlockAssociated(describeVPCOutput *awsec2.DescribeVpcsOutput) error {
+func (v *ExistingVPCResourceSet) checkIPv6CidrBlockAssociated(describeVPCOutput *ec22.DescribeVpcsOutput) error {
 	if len(describeVPCOutput.Vpcs[0].Ipv6CidrBlockAssociationSet) == 0 {
 		return fmt.Errorf("VPC %q does not have any associated IPv6 CIDR blocks", v.clusterConfig.VPC.ID)
 	}
 	return nil
 }
 
-func (v *ExistingVPCResourceSet) importExistingResources() error {
+func (v *ExistingVPCResourceSet) importExistingResources(ctx context.Context) error {
 	if subnets := v.clusterConfig.VPC.Subnets.Private; subnets != nil {
 		var (
 			subnetRoutes map[string]string
 			err          error
 		)
 		if v.isFullyPrivate() {
-			subnetRoutes, err = importRouteTables(v.ec2API, v.clusterConfig.VPC.Subnets.Private)
+			subnetRoutes, err = importRouteTables(ctx, v.ec2API, v.clusterConfig.VPC.Subnets.Private)
 			if err != nil {
 				return err
 			}
@@ -150,35 +155,30 @@ func makeSubnetResources(subnets map[string]api.AZSubnetSpec, subnetRoutes map[s
 	return subnetResources, nil
 }
 
-func importRouteTables(ec2API ec2iface.EC2API, subnets map[string]api.AZSubnetSpec) (map[string]string, error) {
+func importRouteTables(ctx context.Context, ec2API awsapi.EC2, subnets map[string]api.AZSubnetSpec) (map[string]string, error) {
 	var subnetIDs []string
 	for _, subnet := range subnets {
 		subnetIDs = append(subnetIDs, subnet.ID)
 	}
 
-	var routeTables []*ec2.RouteTable
-	var nextToken *string
+	var routeTables []ec2types.RouteTable
 
-	for {
-		output, err := ec2API.DescribeRouteTables(&ec2.DescribeRouteTablesInput{
-			Filters: []*ec2.Filter{
-				{
-					Name:   aws.String("association.subnet-id"),
-					Values: aws.StringSlice(subnetIDs),
-				},
+	paginator := ec22.NewDescribeRouteTablesPaginator(ec2API, &ec22.DescribeRouteTablesInput{
+		Filters: []ec2types.Filter{
+			{
+				Name:   aws.String("association.subnet-id"),
+				Values: subnetIDs,
 			},
-			NextToken: nextToken,
-		})
+		},
+	})
 
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(ctx)
 		if err != nil {
 			return nil, errors.Wrap(err, "error describing route tables")
 		}
 
 		routeTables = append(routeTables, output.RouteTables...)
-
-		if nextToken = output.NextToken; nextToken == nil {
-			break
-		}
 	}
 
 	subnetRoutes := make(map[string]string)

pkg/cfn/manager/tasks.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 
+	ec22 "github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+	"github.com/weaveworks/eksctl/pkg/awsapi"
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/aws/aws-sdk-go-v2/aws"
 
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	iamoidc "github.com/weaveworks/eksctl/pkg/iam/oidc"
@@ -21,12 +24,13 @@ type createClusterTask struct {
 	info                 string
 	stackCollection      *StackCollection
 	supportsManagedNodes bool
+	ctx                  context.Context
 }
 
 func (t *createClusterTask) Describe() string { return t.info }
 
 func (t *createClusterTask) Do(errorCh chan error) error {
-	return t.stackCollection.createClusterTask(errorCh, t.supportsManagedNodes)
+	return t.stackCollection.createClusterTask(t.ctx, errorCh, t.supportsManagedNodes)
 }
 
 type nodeGroupTask struct {
@@ -49,24 +53,26 @@ type managedNodeGroupTask struct {
 	stackCollection   *StackCollection
 	forceAddCNIPolicy bool
 	vpcImporter       vpc.Importer
+	ctx               context.Context
 }
 
 func (t *managedNodeGroupTask) Describe() string { return t.info }
 
 func (t *managedNodeGroupTask) Do(errorCh chan error) error {
-	return t.stackCollection.createManagedNodeGroupTask(errorCh, t.nodeGroup, t.forceAddCNIPolicy, t.vpcImporter)
+	return t.stackCollection.createManagedNodeGroupTask(t.ctx, errorCh, t.nodeGroup, t.forceAddCNIPolicy, t.vpcImporter)
 }
 
 type clusterCompatTask struct {
 	info            string
 	stackCollection *StackCollection
+	ctx             context.Context
 }
 
 func (t *clusterCompatTask) Describe() string { return t.info }
 
 func (t *clusterCompatTask) Do(errorCh chan error) error {
 	defer close(errorCh)
-	return t.stackCollection.FixClusterCompatibility()
+	return t.stackCollection.FixClusterCompatibility(t.ctx)
 }
 
 type taskWithClusterIAMServiceAccountSpec struct {
@@ -139,7 +145,8 @@ func (t *kubernetesTask) Do(errs chan error) error {
 }
 
 type AssignIpv6AddressOnCreationTask struct {
-	EC2API        ec2iface.EC2API
+	EC2API        awsapi.EC2
+	Context       context.Context
 	ClusterConfig *api.ClusterConfig
 }
 
@@ -151,8 +158,8 @@ func (t *AssignIpv6AddressOnCreationTask) Do(errs chan error) error {
 	defer close(errs)
 	if t.ClusterConfig.VPC.Subnets.Public != nil {
 		for _, subnet := range t.ClusterConfig.VPC.Subnets.Public.WithIDs() {
-			_, err := t.EC2API.ModifySubnetAttribute(&ec2.ModifySubnetAttributeInput{
-				AssignIpv6AddressOnCreation: &ec2.AttributeBooleanValue{
+			_, err := t.EC2API.ModifySubnetAttribute(t.Context, &ec22.ModifySubnetAttributeInput{
+				AssignIpv6AddressOnCreation: &ec2types.AttributeBooleanValue{
 					Value: aws.Bool(true),
 				},
 				SubnetId: aws.String(subnet),

coolguy741/weave-eksctl

pkg/cfn/builder/vpc_existing.go
@@ -1,30 +1,33 @@
 package builder
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	awsec2 "github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/pkg/errors"
+	gfnt "github.com/weaveworks/goformation/v4/cloudformation/types"
+
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/awsapi"
 	"github.com/weaveworks/eksctl/pkg/cfn/outputs"
 	"github.com/weaveworks/eksctl/pkg/vpc"
-	gfnt "github.com/weaveworks/goformation/v4/cloudformation/types"
 )
 
 type ExistingVPCResourceSet struct {
 	rs            *resourceSet
 	clusterConfig *api.ClusterConfig
-	ec2API        ec2iface.EC2API
+	ec2API        awsapi.EC2
 	vpcID         *gfnt.Value
 	subnetDetails *SubnetDetails
 }
 
 // NewExistingVPCResourceSet creates and returns a new VPCResourceSet
-func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig, ec2API ec2iface.EC2API) *ExistingVPCResourceSet {
+func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig, ec2API awsapi.EC2) *ExistingVPCResourceSet {
 	return &ExistingVPCResourceSet{
 		rs:            rs,
 		clusterConfig: clusterConfig,
@@ -34,9 +37,9 @@ func NewExistingVPCResourceSet(rs *resourceSet, clusterConfig *api.ClusterConfig
 	}
 }
 
-func (v *ExistingVPCResourceSet) CreateTemplate() (*gfnt.Value, *SubnetDetails, error) {
-	out, err := v.ec2API.DescribeVpcs(&awsec2.DescribeVpcsInput{
-		VpcIds: aws.StringSlice([]string{v.clusterConfig.VPC.ID}),
+func (v *ExistingVPCResourceSet) CreateTemplate(ctx context.Context) (*gfnt.Value, *SubnetDetails, error) {
+	out, err := v.ec2API.DescribeVpcs(ctx, &ec2.DescribeVpcsInput{
+		VpcIds: []string{v.clusterConfig.VPC.ID},
 	})
 
 	if err != nil {
@@ -52,16 +55,16 @@ func (v *ExistingVPCResourceSet) CreateTemplate() (*gfnt.Value, *SubnetDetails, 
 			return nil, nil, err
 		}
 	}
-	if err := v.importExistingResources(); err != nil {
+	if err := v.importExistingResources(ctx); err != nil {
 		return nil, nil, errors.Wrap(err, "error importing VPC resources")
 	}
 
-	v.addOutputs()
+	v.addOutputs(ctx)
 	return v.vpcID, v.subnetDetails, nil
 }
 
 // addOutputs adds VPC resource outputs
-func (v *ExistingVPCResourceSet) addOutputs() {
+func (v *ExistingVPCResourceSet) addOutputs(ctx context.Context) {
 	v.rs.defineOutput(outputs.ClusterVPC, v.vpcID, true, func(val string) error {
 		v.clusterConfig.VPC.ID = val
 		return nil
@@ -73,7 +76,7 @@ func (v *ExistingVPCResourceSet) addOutputs() {
 
 	addSubnetOutput := func(subnetRefs []*gfnt.Value, topology api.SubnetTopology, outputName string) {
 		v.rs.defineJoinedOutput(outputName, subnetRefs, true, func(value string) error {
-			return vpc.ImportSubnetsFromIDList(v.ec2API, v.clusterConfig, topology, strings.Split(value, ","))
+			return vpc.ImportSubnetsFromIDList(ctx, v.ec2API, v.clusterConfig, topology, strings.Split(value, ","))
 		})
 	}
 
@@ -90,21 +93,21 @@ func (v *ExistingVPCResourceSet) addOutputs() {
 	}
 }
 
-func (v *ExistingVPCResourceSet) checkIPv6CidrBlockAssociated(describeVPCOutput *awsec2.DescribeVpcsOutput) error {
+func (v *ExistingVPCResourceSet) checkIPv6CidrBlockAssociated(describeVPCOutput *ec2.DescribeVpcsOutput) error {
 	if len(describeVPCOutput.Vpcs[0].Ipv6CidrBlockAssociationSet) == 0 {
 		return fmt.Errorf("VPC %q does not have any associated IPv6 CIDR blocks", v.clusterConfig.VPC.ID)
 	}
 	return nil
 }
 
-func (v *ExistingVPCResourceSet) importExistingResources() error {
+func (v *ExistingVPCResourceSet) importExistingResources(ctx context.Context) error {
 	if subnets := v.clusterConfig.VPC.Subnets.Private; subnets != nil {
 		var (
 			subnetRoutes map[string]string
 			err          error
 		)
 		if v.isFullyPrivate() {
-			subnetRoutes, err = importRouteTables(v.ec2API, v.clusterConfig.VPC.Subnets.Private)
+			subnetRoutes, err = importRouteTables(ctx, v.ec2API, v.clusterConfig.VPC.Subnets.Private)
 			if err != nil {
 				return err
 			}
@@ -150,35 +153,30 @@ func makeSubnetResources(subnets map[string]api.AZSubnetSpec, subnetRoutes map[s
 	return subnetResources, nil
 }
 
-func importRouteTables(ec2API ec2iface.EC2API, subnets map[string]api.AZSubnetSpec) (map[string]string, error) {
+func importRouteTables(ctx context.Context, ec2API awsapi.EC2, subnets map[string]api.AZSubnetSpec) (map[string]string, error) {
 	var subnetIDs []string
 	for _, subnet := range subnets {
 		subnetIDs = append(subnetIDs, subnet.ID)
 	}
 
-	var routeTables []*ec2.RouteTable
-	var nextToken *string
+	var routeTables []ec2types.RouteTable
 
-	for {
-		output, err := ec2API.DescribeRouteTables(&ec2.DescribeRouteTablesInput{
-			Filters: []*ec2.Filter{
-				{
-					Name:   aws.String("association.subnet-id"),
-					Values: aws.StringSlice(subnetIDs),
-				},
+	paginator := ec2.NewDescribeRouteTablesPaginator(ec2API, &ec2.DescribeRouteTablesInput{
+		Filters: []ec2types.Filter{
+			{
+				Name:   aws.String("association.subnet-id"),
+				Values: subnetIDs,
 			},
-			NextToken: nextToken,
-		})
+		},
+	})
 
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(ctx)
 		if err != nil {
 			return nil, errors.Wrap(err, "error describing route tables")
 		}
 
 		routeTables = append(routeTables, output.RouteTables...)
-
-		if nextToken = output.NextToken; nextToken == nil {
-			break
-		}
 	}
 
 	subnetRoutes := make(map[string]string)

pkg/cfn/manager/tasks.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+	"github.com/weaveworks/eksctl/pkg/awsapi"
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	iamoidc "github.com/weaveworks/eksctl/pkg/iam/oidc"
@@ -21,12 +23,13 @@ type createClusterTask struct {
 	info                 string
 	stackCollection      *StackCollection
 	supportsManagedNodes bool
+	ctx                  context.Context
 }
 
 func (t *createClusterTask) Describe() string { return t.info }
 
 func (t *createClusterTask) Do(errorCh chan error) error {
-	return t.stackCollection.createClusterTask(errorCh, t.supportsManagedNodes)
+	return t.stackCollection.createClusterTask(t.ctx, errorCh, t.supportsManagedNodes)
 }
 
 type nodeGroupTask struct {
@@ -49,24 +52,26 @@ type managedNodeGroupTask struct {
 	stackCollection   *StackCollection
 	forceAddCNIPolicy bool
 	vpcImporter       vpc.Importer
+	ctx               context.Context
 }
 
 func (t *managedNodeGroupTask) Describe() string { return t.info }
 
 func (t *managedNodeGroupTask) Do(errorCh chan error) error {
-	return t.stackCollection.createManagedNodeGroupTask(errorCh, t.nodeGroup, t.forceAddCNIPolicy, t.vpcImporter)
+	return t.stackCollection.createManagedNodeGroupTask(t.ctx, errorCh, t.nodeGroup, t.forceAddCNIPolicy, t.vpcImporter)
 }
 
 type clusterCompatTask struct {
 	info            string
 	stackCollection *StackCollection
+	ctx             context.Context
 }
 
 func (t *clusterCompatTask) Describe() string { return t.info }
 
 func (t *clusterCompatTask) Do(errorCh chan error) error {
 	defer close(errorCh)
-	return t.stackCollection.FixClusterCompatibility()
+	return t.stackCollection.FixClusterCompatibility(t.ctx)
 }
 
 type taskWithClusterIAMServiceAccountSpec struct {
@@ -139,7 +144,8 @@ func (t *kubernetesTask) Do(errs chan error) error {
 }
 
 type AssignIpv6AddressOnCreationTask struct {
-	EC2API        ec2iface.EC2API
+	EC2API        awsapi.EC2
+	Context       context.Context
 	ClusterConfig *api.ClusterConfig
 }
 
@@ -151,8 +157,8 @@ func (t *AssignIpv6AddressOnCreationTask) Do(errs chan error) error {
 	defer close(errs)
 	if t.ClusterConfig.VPC.Subnets.Public != nil {
 		for _, subnet := range t.ClusterConfig.VPC.Subnets.Public.WithIDs() {
-			_, err := t.EC2API.ModifySubnetAttribute(&ec2.ModifySubnetAttributeInput{
-				AssignIpv6AddressOnCreation: &ec2.AttributeBooleanValue{
+			_, err := t.EC2API.ModifySubnetAttribute(t.Context, &ec2.ModifySubnetAttributeInput{
+				AssignIpv6AddressOnCreation: &ec2types.AttributeBooleanValue{
 					Value: aws.Bool(true),
 				},
 				SubnetId: aws.String(subnet),